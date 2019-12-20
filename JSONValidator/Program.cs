f5601083c1a5342010611cd2b172e45e35e40417
Cipri <cipri91rusu@gmail.com>
2019-12-20 15:38:41 +0200

    On branch master 	new file:   JSONValidator/JSONValidator.csproj 	new file:   JSONValidator/JSONValidator.sln 	new file:   JSONValidator/Program.cs 	new file:   JSONValidatorTest/JSONValidatorTest.csproj 	new file:   JSONValidatorTest/UnitTest1.cs

---
+++ JSONValidator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JSONValidator
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        public static bool ValidateJson(string inputTestValue)
+        {
+            return true;
+        }
+    }
+}
