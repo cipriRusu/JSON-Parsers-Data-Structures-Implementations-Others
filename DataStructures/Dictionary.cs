4548a0f35b3f3f13aeddf2dae6d7d82341b5566b
Cipri Rusu <cipri91rusu@gmail.com>
2020-03-18 09:21:42 +0200

    Began Dictionary Implementation 	new file:   DataStructures/Dictionary.cs 	new file:   DataStructures/DictionaryTest.cs

---
+++ DataStructures/Dictionary.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Dictionary<TKey, Tvalue> : IDictionary<TKey, Tvalue>
+    {
+        private int[] buckets;
+
+        private Element[] elements;
+
+        private int length;
+
+        public Dictionary(int length = 0)
+        {
+            this.length = length;
+            buckets = new int[length];
+            Array.Fill(buckets, -1);
+            elements = new Element[length];
+            Count = 0;
+        }
+
+        public Tvalue this[TKey key]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public ICollection<TKey> Keys => throw new NotImplementedException();
+
+        public ICollection<Tvalue> Values => throw new NotImplementedException();
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(TKey key, Tvalue value)
+        {
+            var sourceBucketIndex = Math.Abs(key.GetHashCode() % length);
+            var sourceElementIndex = 0;
+
+            elements[sourceElementIndex].key = key;
+            elements[sourceElementIndex].value = value;
+            elements[sourceElementIndex].Next = buckets[sourceBucketIndex];
+            buckets[sourceBucketIndex] = sourceElementIndex;
+        }
+
+        public void Add(KeyValuePair<TKey, Tvalue> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public void Clear()
+        {
+            Array.Fill(buckets, -1);
+            Array.Clear(elements, 0, Count);
+            Count = 0;
+        }
+
+        public bool Contains(KeyValuePair<TKey, Tvalue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, Tvalue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, Tvalue>> GetEnumerator()
+        {
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                for(int j = buckets[i]; j != -1; j = elements[j].Next)
+                {
+                    yield return new KeyValuePair<TKey, Tvalue>(elements[j].key, elements[j].value);
+                }
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, Tvalue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out Tvalue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private struct Element
+        {
+            public TKey key { get; set; }
+            public int Next { get; set; }
+            public Tvalue value { get; set; }
+        }
+    }
+}
---
+++ DataStructures/DictionaryTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructures
+{
+    public class DictionaryTest
+    {
+        [Fact]
+        public void DictionaryInitializesNewEmptyDictionary()
+        {
+            var testDict = new Dictionary<int, string>();
+
+            Assert.Empty(testDict);
+        }
+
+        [Fact]
+        public void DictionaryAddInitializesSingleElementDictionary()
+        {
+            var testDict = new Dictionary<int, string>(5);
+
+            testDict.Add(1, "a");
+
+            Assert.Single(testDict);
+        }
+
+        [Fact]
+        public void DictionaryAddInitializezSingleElementDictionaryKVP()
+        {
+            var testDict = new Dictionary<int, string>(10);
+
+            var kvp = new KeyValuePair<int, string>(1, "a");
+
+            testDict.Add(kvp);
+
+            Assert.Single(testDict);
+        }
+
+        [Fact]
+        public void DictionaryClearEmptiesDictionary()
+        {
+            var testDict = new Dictionary<int, string>(5);
+
+            testDict.Add(3, "first");
+            testDict.Add(4, "second");
+            testDict.Add(2, "third");
+
+            testDict.Clear();
+
+            Assert.Empty(testDict);
+        }
+    }
+}
