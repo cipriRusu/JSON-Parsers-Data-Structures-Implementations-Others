84ba4e6374f43112ac01b3fea4c5ffe9eb1c16ad
Cipri Rusu <cipri91rusu@gmail.com>
2020-03-25 08:59:23 +0200

    Started BinaryTree Implementation new file:   DataStructures/BinaryTree.cs new file:   DataStructures/BinaryTreeTest.cs

---
+++ DataStructures/BinaryTree.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DataStructures
+{
+    public class BinaryTree<T> : ICollection<T> where T : IComparable<T>
+    {
+        private TreeNode<T> Root;
+
+        public BinaryTree()
+        {
+            Root = null;
+            Count = 0;
+        }
+
+        public bool IsReadOnly => false;
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            var current = Root;
+
+            if (current == null)
+            {
+                Root = new TreeNode<T>(item);
+                Count++;
+            }
+            else
+            {
+                AddNode(current, item);
+            }
+        }
+
+        private void AddNode(TreeNode<T> node, T value)
+        {
+            if (node.NodeValue.CompareTo(value) > 0)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = new TreeNode<T>(value);
+                }
+                else
+                {
+                    AddNode(node.Left, value);
+                }
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new TreeNode<T>(value);
+                }
+                else
+                {
+                    AddNode(node.Right, value);
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Root;
+
+            if (current == null)
+            {
+                yield break;
+            }
+
+            yield return current.NodeValue;
+
+            if (current.Left != null)
+            {
+                yield return current.Left.NodeValue;
+            }
+
+            if (current.Right != null)
+            {
+                yield return current.Right.NodeValue;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class TreeNode<T> : IComparable<T> where T : IComparable<T>
+        {
+            public TreeNode<T> Left;
+            public T NodeValue;
+            public TreeNode<T> Right;
+            public TreeNode(T value = default)
+            {
+                Left = null;
+                Right = null;
+                NodeValue = value;
+            }
+
+            public int CompareTo([AllowNull] T other)
+            {
+                return NodeValue.CompareTo(other);
+            }
+        }
+    }
+}
---
+++ DataStructures/BinaryTreeTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructures
+{
+    public class BinaryTreeTest
+    {
+        [Fact]
+        public void BinaryTreeInitializesEmptyBinaryTree()
+        {
+            //Null binary tree is a valid, empty binary tree
+            var binTree = new BinaryTree<int>();
+
+            Assert.Empty(binTree);
+        }
+
+        [Fact]
+        public void BinaryTreePopulatesTreeWithSingleElement()
+        {
+            var binTree = new BinaryTree<int>();
+
+            binTree.Add(3);
+
+            Assert.Single(binTree);
+        }
+
+        [Fact]
+        public void BinaryTreeContainsReturnsTrueForValidRootValue()
+        {
+            var binTree = new BinaryTree<int>();
+
+            binTree.Add(7);
+            binTree.Add(2);
+            binTree.Add(10);
+
+            Assert.Contains(7, binTree);
+        }
+
+        [Fact]
+        public void BinaryTreeContainsReturnsTrueForValidLowerThanRootValue()
+        {
+            var binTree = new BinaryTree<int>();
+
+            binTree.Add(7);
+            binTree.Add(2);
+            binTree.Add(10);
+
+            Assert.Contains(2, binTree);
+        }
+
+        [Fact]
+        public void BinaryTreeContainsReturnsTrueForValidBiggerThanRootValue()
+        {
+            var binTree = new BinaryTree<int>();
+
+            binTree.Add(7);
+            binTree.Add(2);
+            binTree.Add(10);
+
+            Assert.Contains(10, binTree);
+        }
+
+        [Fact]
+        public void BinaryTreeContainsReturnsTrueForPresentValueInThirdLevel()
+        {
+            var binTree = new BinaryTree<int>();
+
+            binTree.Add(3);
+            binTree.Add(2);
+            binTree.Add(1);
+
+            Assert.Contains(1, binTree);
+        }
+
+        [Fact]
+        public void BinaryTreeContainsReturnsTrueForPresnetValueNestedInBranch()
+        {
+            var BinTree = new BinaryTree<int>();
+
+            BinTree.Add(10);
+            BinTree.Add(1);
+            BinTree.Add(6);
+            BinTree.Add(3);
+
+            Assert.Contains(3, BinTree);
+        }
+    }
+}
