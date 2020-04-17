96ed3b5f2535d31a6f2121e349ba1dea1c40dea6
Cipri Rusu <cipri91rusu@gmail.com>
2020-04-17 22:21:08 +0300

    Started LINQ Methods implementation 	new file:   Functional-LINQ/Functional-LINQ.csproj 	new file:   Functional-LINQ/Functional-LINQ.sln 	new file:   Functional-LINQ/FunctionalLINQ.cs 	new file:   Functional-LINQ/FunctionalLINQTest.cs

---
+++ Functional-LINQ/FunctionalLINQ.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Functional_LINQ
+{
+    public static class FunctionalLINQ
+    {
+        public static bool All<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            foreach (var element in source)
+            {
+                if (!predicate(element))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Any<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static TSource First<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                {
+                    return element;
+                }
+            }
+
+            throw new InvalidOperationException("Invalid Operation");
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TResult> selector)
+        {
+            foreach (var element in source)
+            {
+                yield return selector(element);
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
+        {
+            foreach (var element in source)
+            {
+                foreach (var item in selector(element))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> Where<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        public static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(
+    this IEnumerable<TSource> source,
+    Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector)
+        {
+            var outputDictionary = new Dictionary<TKey, TElement>();
+
+            foreach (var value in source)
+            {
+                outputDictionary.Add(keySelector(value), elementSelector(value));
+            }
+
+            return outputDictionary;
+        }
+
+        public static IEnumerable<TResult> Zip<TFirst, TSecond, TResult>(
+    this IEnumerable<TFirst> first, IEnumerable<TSecond> second,
+    Func<TFirst, TSecond, TResult> resultSelector)
+        {
+            var firstEnumerator = first.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+
+            while (firstEnumerator.MoveNext() && secondEnumerator.MoveNext())
+            {
+                yield return resultSelector(firstEnumerator.Current, secondEnumerator.Current);
+            }
+        }
+
+        public static TAccumulate Aggregate<TSource, TAccumulate>(
+    this IEnumerable<TSource> source, TAccumulate seed,
+    Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            foreach (var element in source)
+            {
+                seed = func(seed, element);
+            }
+
+            return seed;
+        }
+
+        public static IEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(
+    this IEnumerable<TOuter> outer, IEnumerable<TInner> inner,
+    Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector,
+    Func<TOuter, TInner, TResult> resultSelector)
+        {
+            foreach (var element in outer)
+            {
+                foreach (var inside in inner)
+                {
+                    if (outerKeySelector(element).Equals(innerKeySelector(inside)))
+                    {
+                        yield return resultSelector(element, inside);
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> Distinct<TSource>(
+    this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
+        {
+            var containedSet = new HashSet<TSource>(comparer);
+
+            foreach (var element in source)
+            {
+                if (containedSet.Add(element))
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> Union<TSource>(
+    this IEnumerable<TSource> first, IEnumerable<TSource> second,
+    IEqualityComparer<TSource> comparer)
+        {
+            var containedSet = new HashSet<TSource>(comparer);
+
+            foreach (var element in first)
+            {
+                if (containedSet.Add(element))
+                {
+                    yield return element;
+                }
+            }
+
+            foreach (var element in second)
+            {
+                if (containedSet.Add(element))
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> Intersect<TSource>(
+    this IEnumerable<TSource> first, IEnumerable<TSource> second,
+    IEqualityComparer<TSource> comparer)
+        {
+            foreach (var firstITem in first)
+            {
+                foreach (var secondItem in second)
+                {
+                    if (comparer.Equals(firstITem, secondItem))
+                    {
+                        yield return firstITem;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> Except<TSource>(
+    this IEnumerable<TSource> first, IEnumerable<TSource> second,
+    IEqualityComparer<TSource> comparer)
+        {
+            foreach (var firstElement in first)
+            {
+                bool isPresent = false;
+
+                foreach (var secondElement in second)
+                {
+                    if (comparer.Equals(firstElement, secondElement))
+                    {
+                        isPresent = true;
+                        break;
+                    }
+                }
+
+                if (!isPresent)
+                {
+                    yield return firstElement;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> GroupBy<TSource, TKey, TElement, TResult>
+        (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector,
+        Func<TSource, TElement> elementSelector,
+        Func<TKey, IEnumerable<TElement>, TResult> resultSelector,
+        IEqualityComparer<TKey> comparer)
+        {
+            var dictionary = new Dictionary<TKey, List<TElement>>(comparer);
+
+            foreach(var element in source)
+            {
+                var key = keySelector(element);
+                var value = elementSelector(element);
+
+                if (!dictionary.ContainsKey(key))
+                {
+                    dictionary.Add(key, new List<TElement>() { value });
+                }
+                else
+                {
+                    dictionary[key].Add(value);
+                }
+            }
+
+            foreach (var element in dictionary.Keys)
+            {
+                yield return resultSelector(element, dictionary[element].ToList());
+            }
+        }
+    }
+}
---
+++ Functional-LINQ/FunctionalLINQTest.cs
@@ -0,0 +1,292 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Functional_LINQ
+{
+    public class FunctionalLINQTest
+    {
+        [Fact]
+        public void AllMethodReturnsTrueForAllElementsValidForPredicate()
+        {
+            var collection = new int[] { 1, 3, 4, 9, 7, 2 };
+            Assert.True(FunctionalLINQ.All(collection, x => x < 10));
+        }
+
+        [Fact]
+        public void AllMethodReturnsFalseForSingleElementNotValidInPredicate()
+        {
+            var collection = new int[] { 1, 3, 4, 9, 7, 12 };
+            Assert.False(FunctionalLINQ.All(collection, x => x < 10));
+        }
+
+        [Fact]
+        public void AnyMethodReturnTrueForSingleElementValidForPredicate()
+        {
+            var collection = new int[] { 2, 1, 9, 4, 7 };
+            Assert.True(FunctionalLINQ.Any(collection, x => x == 9));
+        }
+
+        [Fact]
+        public void AnyMethodReturnFalseForNoElementValidForPredicate()
+        {
+            var collection = new int[] { 2, 1, 0, 9, 4 };
+            Assert.False(FunctionalLINQ.Any(collection, x => x == 12));
+        }
+
+        [Fact]
+        public void FirstReturnsTrueForFirstElementThatSatisfiesCondition()
+        {
+            var collection = new int[] { 2, 1, 0, 5, 3, 8 };
+            Assert.Equal(5, FunctionalLINQ.First(collection, x => x == 5));
+        }
+
+        [Fact]
+        public void FirstThrowsInvalidOperationExceptionForAbsentValue()
+        {
+            var collection = new int[] { 2, 1, 0, 4, 9 };
+            Assert.Throws<InvalidOperationException>(() => FunctionalLINQ.First(collection, x => x == 3));
+        }
+
+        [Fact]
+        public void SelectReturnValidOutputForInputedParameters()
+        {
+            var collection = new int[] { 2, 1, 0, 9, 4 };
+            var expected = new int[] { 4, 2, 0, 18, 8 };
+            Assert.Equal(expected, FunctionalLINQ.Select(collection, x => x * 2));
+        }
+
+        [Fact]
+        public void SelectManyReturnsValidOutputForInputedParameters()
+        {
+            var collection = new int[][] { new int[] { 1, 3 }, new int[] { 4 }, new int[] { 7, 8, 9 } };
+            var expected = new int[] { 1, 3, 4, 7, 8, 9 };
+            Assert.Equal(expected, FunctionalLINQ.SelectMany(collection, x => x));
+        }
+
+        [Fact]
+        public void WhereReturnsValidOutputForInputedParameters()
+        {
+            var collection = new int[] { 1, 3, 0, 9, 4, 7 };
+            var expected = new int[] { 9, 7 };
+            Assert.Equal(expected, FunctionalLINQ.Where(collection, x => x > 5));
+        }
+
+        [Fact]
+        public void ToDictionaryReturnsValidOuputForInputedParameters()
+        {
+            var collection = new int[] { 0, 1, 2, 3, 4, 5 };
+            var expected = new Dictionary<int, string>();
+
+            expected.Add(0, "test");
+            expected.Add(1, "test");
+            expected.Add(2, "test");
+            expected.Add(3, "test");
+            expected.Add(4, "test");
+            expected.Add(5, "test");
+
+            Assert.Equal(expected, FunctionalLINQ.ToDictionary(collection, x => x, x => "test"));
+        }
+
+        [Fact]
+        public void ZipReturnsValidOutputForInputParameters()
+        {
+            var firstCollection = new int[] { 0, 1, 8, 9, 4 };
+            var secondCollection = new string[] { "zero", "one", "eight", "nine", "four" };
+
+            var expected = new string[]
+            {
+                "0 - zero",
+                "1 - one",
+                "8 - eight",
+                "9 - nine",
+                "4 - four"
+            };
+
+            Assert.Equal(expected, FunctionalLINQ.Zip(firstCollection, secondCollection,
+            (firstCollection, secondCollection) => firstCollection + " - " + secondCollection));
+        }
+
+        [Fact]
+        public void ZipReturnValidOuputForInputParametersTupledOuput()
+        {
+            var firstCollection = new int[] { 0, 1, 4 };
+            var secondCollection = new string[] { "zero", "one", "four" };
+            var expected = new (int, string)[]
+            {
+                (0, "zero"),
+                (1, "one"),
+                (4, "four")
+            };
+
+            Assert.Equal(expected, FunctionalLINQ.Zip(firstCollection, secondCollection,
+            (firstCollection, secondCollection) => (firstCollection, secondCollection)));
+        }
+
+        [Fact]
+        public void ZipReturnValidOuputForFirstShorterCollection()
+        {
+
+            var firstCollection = new int[] { 1, 2, 3 };
+            var secondCollection = new string[] { "first", "second", "third", "fourth", "fifth" };
+
+            var expected = new string[]
+            {
+                "1 - first",
+                "2 - second",
+                "3 - third"
+            };
+
+            Assert.Equal(expected, FunctionalLINQ.Zip(firstCollection, secondCollection,
+            (firstCollection, secondCollection) => firstCollection + " - " + secondCollection));
+        }
+
+        [Fact]
+        public void ZipReturnValidOuputForSecondShorterCollection()
+        {
+
+            var firstCollection = new int[] { 1, 2, 3, 4, 5 };
+            var secondCollection = new string[] { "first", "second", "third" };
+
+            var expected = new string[]
+            {
+                "1 - first",
+                "2 - second",
+                "3 - third"
+            };
+
+            Assert.Equal(expected, FunctionalLINQ.Zip(firstCollection, secondCollection,
+            (firstCollection, secondCollection) => firstCollection + " - " + secondCollection));
+        }
+
+        [Fact]
+        public void AggregateReturnsValidOutputForValidInput()
+        {
+            var collection = new int[] { 1, 2, 3, 4, 5 };
+
+            int expected = 15;
+
+            Assert.Equal(expected,
+            FunctionalLINQ.Aggregate(collection, 0, (x, y) => x + y));
+        }
+
+        [Fact]
+        public void JoinReturnsValidOuputForValidInput()
+        {
+            var first = new int[] { 1, 2, 3 };
+            var sec = new int[] { 2, 3, 4 };
+
+            var expected = new int[] { 2, 3 };
+
+            Assert.Equal(expected,
+            FunctionalLINQ.Join(first, sec, first => first, sec => sec, (first, sec) => first));
+        }
+
+        [Fact]
+        public void DistinctReturnsValidOuputForValidInput()
+        {
+            var collection = new int[] { 1, 2, 1, 7, 9, 1, 3 };
+            var expected = new int[] { 1, 2, 7, 9, 3 };
+
+            var result = collection.Distinct(new EqualityComparer<int>());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void UnionReturnsValidOutputForValidInput()
+        {
+            var firstCollection = new int[] { 2, 1, 9, 0 };
+            var secCollecton = new int[] { 2, 4, 3, 9, 1 };
+            IEnumerable expected = new int[] { 2, 1, 9, 0, 4, 3 };
+
+            IEnumerable result = firstCollection.Union(secCollecton, new EqualityComparer<int>());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void IntersectReturnsValidOutputForValidInput()
+        {
+            var firstCollection = new int[] { 1, 2, 0, 4 };
+            var secCollection = new int[] { 3, 2, 0, 9 };
+
+            IEnumerable expected = new int[] { 2, 0 };
+
+            IEnumerable result = firstCollection.Intersect(secCollection, new EqualityComparer<int>());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ExceptReturnsValidOutputForValidInput()
+        {
+            var firstCollection = new int[] { 1, 2, 0, 4 };
+            var secCollection = new int[] { 3, 2, 0, 9 };
+
+            IEnumerable expected = new int[] { 1, 4 };
+            IEnumerable result = firstCollection.Except(secCollection, new EqualityComparer<int>());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GroupByReturnsValidOutputForValidInput()
+        {
+            var current = new Dictionary<string, int>();
+
+            current.Add("first", 1);
+            current.Add("second", 2);
+            current.Add("third", 1);
+            current.Add("fourth", 2);
+
+            var actual = current.GroupBy(x => x.Value, y => y.Key,
+                (key, value) => new Dictionary<int, List<string>>()
+                {
+                    [key] = new List<string>(value)
+                },
+                new EqualityComparer<int>());
+
+            var actualData = ExtractData(actual);
+
+            var expected = new Dictionary<int, List<string>>()
+            {
+                [1] = new List<string>() { "first", "third" },
+                [2] = new List<string>() { "second", "fourth" }
+            };
+
+            Assert.Equal(expected, actualData);
+        }
+
+        private Dictionary<int, List<string>> ExtractData(IEnumerable<Dictionary<int, List<string>>> actual)
+        {
+            var TotalElements = new Dictionary<int, List<string>>();
+
+            foreach (var element in actual)
+            {
+                foreach (var listElement in element)
+                {
+                    TotalElements.Add(listElement.Key, listElement.Value);
+                }
+            }
+
+            return TotalElements;
+        }
+
+        public class EqualityComparer<T> : IEqualityComparer<T>
+        {
+            public bool Equals([AllowNull] T x, [AllowNull] T y)
+            {
+                return x.Equals(y);
+            }
+
+            public int GetHashCode([DisallowNull] T obj)
+            {
+                return GetHashCode();
+            }
+        }
+    }
+
+}
