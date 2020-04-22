c330ba87a8011ce4506a338f0b5d014f42d77543
Cipri Rusu <cipri91rusu@gmail.com>
2020-04-22 12:52:18 +0300

    Implemented Product Class to use with Stock (+ Unit tests) 	new file:   Functional-LINQ/StockProject/Product.cs 	new file:   Functional-LINQ/StockProject/Stock.cs 	new file:   Functional-LINQ/StockProject/StockTest.cs

---
+++ Functional-LINQ/StockProject/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional_LINQ.StockProject
+{
+    public class Product
+    {
+        public string ProductName { get; private set; }
+        public int ProductCount { get; private set; }
+
+        public void SetNameAndCount(string productName, int productCount)
+        {
+            NameAndCountExceptions(productName, productCount);
+
+            ProductName = productName;
+            ProductCount = productCount;
+        }
+
+        public void UpdateCounter(int newValue) => ProductCount += newValue;
+
+        private static void NameAndCountExceptions
+            (string productName, int productCount)
+        {
+            if (string.IsNullOrEmpty(productName))
+            {
+                throw new ArgumentNullException("Product name was not valid");
+            }
+
+            if (productCount < 0)
+            {
+                throw new ArgumentException("Product Count is less than 0");
+            }
+        }
+    }
+}
---
+++ Functional-LINQ/StockProject/StockTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Functional_LINQ.StockProject
+{
+    public class StockTest
+    {
+        [Fact]
+        public void CreatesProductClassAndPopulatesName()
+        {
+            var product = new Product();
+            product.SetNameAndCount("Bere", 3);
+
+            Assert.Equal("Bere", product.ProductName);
+        }
+
+        [Fact]
+        public void CreatesProductclassAndPopulatesQuantity()
+        {
+            var product = new Product();
+            product.SetNameAndCount("Bere", 4);
+
+            Assert.Equal(4, product.ProductCount);
+        }
+
+        [Fact]
+        public void CreatesProductClassThrowsArgumentExceptionForNullName()
+        {
+            var product = new Product();
+
+            Assert.Throws<ArgumentNullException>(() => product.SetNameAndCount(null, 4));
+        }
+
+        [Fact]
+        public void CreatesProductclassThrowsArgumentExceptionForNegativeQuantity()
+        {
+            var product = new Product();
+
+            Assert.Throws<ArgumentException>(() => product.SetNameAndCount("Mici", -3));
+        }
+
+        [Fact]
+        public void CreatesProductClassUpdatesCounterForNewUpdatedValue()
+        {
+            var product = new Product();
+
+            product.SetNameAndCount("Bere", 3);
+            product.UpdateCounter(9);
+
+            Assert.Equal(12, product.ProductCount);
+        }
+    }
+}
