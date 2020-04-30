771fa325971f491a5cbea0a90fbf53bf83b6b960
Cipri Rusu <cipri91rusu@gmail.com>
2020-04-30 16:31:14 +0300

    Implemented method for selection of character

---
+++ Functional-LINQ/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Functional_LINQ.FirstNonRepeatingCharacter
+{
+    internal class FirstNonRepeatingCharacter
+    {
+        public char FirstNonRepChar { get; internal set; }
+
+        private string input;
+
+        public FirstNonRepeatingCharacter(string input)
+        {
+            if (input != null)
+            {
+                this.input = input;
+                FindFirstNonRepeatingCharacter();
+            }
+            else
+            {
+                throw new ArgumentNullException("Input value was null");
+            }
+        }
+
+        private void FindFirstNonRepeatingCharacter()
+        {
+            var buffer = input.GroupBy(x => x).ToDictionary(x => x, x => x.Count())
+                .SkipWhile(x => x.Value != 1).ToDictionary(x => x.Key, y => y.Value);
+
+            FirstNonRepChar = buffer.Keys.First().Key;
+        }
+    }
+}
---
+++ Functional-LINQ/FirstNonRepeatingCharacter/FirstNonRepeatingCharacterTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Functional_LINQ.FirstNonRepeatingCharacter
+{
+    public class FirstNonRepeatingCharacterTest
+    {
+        [Fact]
+        public void FirstNonRepeatingCharacterThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => 
+            new FirstNonRepeatingCharacter(null));
+        }
+
+        [Fact]
+        public void FNRCReturnsValidCharForSingleCharacter()
+        {
+            var FNRC = new FirstNonRepeatingCharacter("a");
+            Assert.Equal('a', FNRC.FirstNonRepChar);
+        }
+
+        [Fact]
+        public void FNRCReturnsCharForValidInput()
+        {
+            var FNRC = new FirstNonRepeatingCharacter("aba");
+            Assert.Equal('b', FNRC.FirstNonRepChar);
+        }
+
+        [Fact]
+        public void FNCRReturnsCharForValidLongerInput()
+        {
+            var FNRC = new FirstNonRepeatingCharacter("halelujah");
+            Assert.Equal('e', FNRC.FirstNonRepChar);
+        }
+    }
+}
