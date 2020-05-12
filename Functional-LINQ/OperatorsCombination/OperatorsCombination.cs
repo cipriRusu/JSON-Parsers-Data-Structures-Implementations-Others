34abdbca833794240755f47c4cb2bd6e52cce0fb
Cipri Rusu <cipri91rusu@gmail.com>
2020-05-12 16:54:07 +0300

    Extracted multiple methods

--- Functional-LINQ/OperatorsCombination/OperatorsCombination.cs
+++ Functional-LINQ/OperatorsCombination/OperatorsCombination.cs
@@ -15,22 +15,33 @@ namespace Functional_LINQ.OperatorsCombination
 
         internal void GenerateOperators(int tailValue, int targetValue)
         {
-            IEnumerable<string> Operators = Enumerable.Range(1, tailValue)
-                .Aggregate((IEnumerable<string>)new string[] { "" },
-                (x, y) => x.SelectMany(r => new string[] { r + "+", r + "-" }));
+            IEnumerable<string> CombinedOperators = OperatorPermutations(tailValue);
+            IEnumerable<string[]> Expressions = GenerateExpressions(tailValue, CombinedOperators);
+            IEnumerable<int> ValidIndexes = IdentifyValidIndexPositions(targetValue, Expressions);
 
-            IEnumerable<string[]> Expressions = Operators.Select((stringValue) =>
-            stringValue.Zip(Enumerable.Range(1, tailValue), (x, y) => x.ToString() + y))
-                .Select(x => x.ToArray());
-
-            IEnumerable<int> ValidIndexes = Results(Expressions).Select((x, y) => new { Item = x, Index = y })
-                .Where(f => f.Item == targetValue).Select(x => x.Index);
-
-            ValidCombinations =  ValidIndexes.Select(x => Expressions.ElementAt(x))
+            ValidCombinations = ValidIndexes.Select(x => Expressions.ElementAt(x))
                 .Select(x => x.Aggregate("", (x, y) => x + y) + $" = {targetValue}\n");
         }
 
-        private IEnumerable<int> Results(IEnumerable<string[]> output)
+        private IEnumerable<int> IdentifyValidIndexPositions(int targetValue, IEnumerable<string[]> Expressions)
+        {
+            return ComputeExpressionResults(Expressions).Select((x, y) =>
+            new { Item = x, Index = y }).Where(f => f.Item == targetValue).Select(x => x.Index);
+        }
+
+        private static IEnumerable<string[]> GenerateExpressions(int tailValue, IEnumerable<string> CombinedOperators)
+        {
+            return CombinedOperators.Select((stringValue) =>
+                        stringValue.Zip(Enumerable.Range(1, tailValue), (x, y) => x.ToString() + y)).Select(x => x.ToArray());
+        }
+
+        private static IEnumerable<string> OperatorPermutations(int tailValue)
+        {
+            return Enumerable.Range(1, tailValue).Aggregate((IEnumerable<string>)new string[] { "" },
+                (currentCombination, y) => currentCombination.SelectMany(r => new string[] { r + "+", r + "-" }));
+        }
+
+        private IEnumerable<int> ComputeExpressionResults(IEnumerable<string[]> output)
         {
             return output.Select(x => x.Aggregate(0, (x, y) => y[0] == '+' ?
             x + Convert.ToInt32(y.Substring(1)) : x - Convert.ToInt32(y.Substring(1))));
