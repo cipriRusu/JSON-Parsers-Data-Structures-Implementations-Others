45fbb2ae3c228d65748dba5c75a8ff60d3188f15
Cipri Rusu <cipri91rusu@gmail.com>
2020-05-13 11:04:36 +0300

    Started implementation for PythaghoreicTriple (Incomplete)

---
+++ Functional-LINQ/PythaghoreicTriple/PythaghoreicTripleTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+
+namespace Functional_LINQ.PythaghoreicTriple
+{
+    public class PythaghoreicTripleTest
+    {
+        [Fact]
+        public void PythaghoreicTripleReturnsEmptyForInvalidTriplet()
+        {
+            int first = 1;
+            int second = 2;
+            int third = 3;
+
+            var triplet = new PythagoreicTriple();
+
+            triplet.GeneratePythaghoreicTriple(first, second, third);
+
+            Assert.Empty(triplet.PythagoreicTriples);
+        }
+
+        [Fact]
+        public void PythagoreicTripleReturnsSingleTripletForValidValue()
+        {
+            int first = 2;
+            int second = 4;
+            int third = 6;
+
+            var triplet = new PythagoreicTriple();
+
+            triplet.GeneratePythaghoreicTriple(first, second, third);
+
+            var expected = new int[][]
+            {
+                new int[]{2, 4, 6 },
+            };
+
+            Assert.Contains(expected, triplet.PythagoreicTriples);
+        }
+    }
+}
