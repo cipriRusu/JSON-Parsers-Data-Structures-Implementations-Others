e1a764e5f222784f0b5576a25493989f0153ddba
Cipri Rusu <cipri91rusu@gmail.com>
2020-05-14 11:52:12 +0300

    Implemented Pythagorhean triples (Incomplete)

--- Functional-LINQ/PythagorheicTriple/PythaghoreicTripleTest.cs
+++ Functional-LINQ/PythagorheicTriple/PythaghoreicTripleTest.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using System.Linq;
+using System.Net.Http.Headers;
 
 namespace Functional_LINQ.PythagorheicTriple
 {
     public class PythaghoreicTripleTest
     {
+        [Fact]
+        public void PythagorheicTripleThrowsArgumentExceptionForZeroValue()
+        {
+            int first = 0;
+            int second = 1;
+            int third = 4;
+
+            Assert.Throws<ArgumentException>(() =>
+            new PythagorheicTriple().GeneratePythaghoreicTriple(first, second, third));
+        }
+
         [Fact]
         public void PythaghoreicTripleReturnsEmptyForInvalidTriplet()
         {
@@ -23,22 +35,47 @@ namespace Functional_LINQ.PythagorheicTriple
         }
 
         [Fact]
-        public void PythagoreicTripleReturnsSingleTripletForValidValue()
+        public void PythagorheicTripleReturnsMultipleValuesForValidInput()
         {
-            int first = 2;
+            int first = 3;
             int second = 4;
-            int third = 6;
+            int third = 5;
 
             var triplet = new PythagorheicTriple();
 
-            triplet.GeneratePythaghoreicTriple(first, second, third);
-
-            var expected = new int[][]
+            IEnumerable<int[]> expected = new List<int[]>
             {
-                new int[]{2, 4, 6 },
+                new int[]{3, 4, 5},
+                new int[]{4, 3, 5},
+                new int[]{5, 3, 4},
+                new int[]{3, 5, 4}
             };
 
-            Assert.Contains(expected, triplet.PythagoreicTriples);
+            triplet.GeneratePythaghoreicTriple(first, second, third);
+
+            Assert.Equal(expected, triplet.PythagoreicTriples);
+        }
+
+        [Fact]
+        public void PythagorheicTripleReturnsMultipleValuesForValidInputLargerValues()
+        {
+            int first = 12;
+            int second = 35;
+            int third = 37;
+
+            var triplet = new PythagorheicTriple();
+
+            IEnumerable<int[]> expected = new List<int[]>
+            {
+                new int[]{12, 35, 37},
+                new int[]{4, 3, 5},
+                new int[]{5, 3, 4},
+                new int[]{3, 5, 4}
+            };
+
+            triplet.GeneratePythaghoreicTriple(first, second, third);
+
+            Assert.Equal(expected, triplet.PythagoreicTriples);
         }
     }
 }
--- Functional-LINQ/PythagorheicTriple/PythagorheicTriple.cs
+++ Functional-LINQ/PythagorheicTriple/PythagorheicTriple.cs
@@ -7,17 +7,30 @@ namespace Functional_LINQ.PythagorheicTriple
 {
     internal class PythagorheicTriple
     {
-        public PythagorheicTriple() => PythagoreicTriples = Enumerable.Empty<int[][]>();
-        public IEnumerable<int[][]> PythagoreicTriples { get; set; }
-        public IEnumerable<IEnumerable<int>> PythaghoreicTriples { get; private set; }
+        public PythagorheicTriple() => PythagoreicTriples = Enumerable.Empty<int[]>();
+        public IEnumerable<int[]> PythagoreicTriples { get; set; }
 
         public void GeneratePythaghoreicTriple(int first, int second, int third)
         {
+            ArgumentExceptions(first, second, third);
+
             var input = new int[] { first, second, third };
 
-            var AllCombinations = input.Select(x => Enumerable.Repeat(x, 1).Union(input))
-                .Select(x => new { firstPermutation = x, 
-                    secondPermutation = x.Take(1).Union(x.Skip(1).Reverse()).ToArray()}).ToArray();
+            var AllCombinations =
+                input.Select(x => Enumerable.Repeat(x, 1).Union(input)).Select(x =>
+                new { f = x.ToArray(), s = x.Take(1).Union(x.Skip(1).Reverse()).ToArray() });
+
+            PythagoreicTriples = 
+                AllCombinations.Select(x => x.f).Concat(AllCombinations.Select(x => x.s))
+                .Where(x => (x[0] ^ 2).Equals(x[1] ^ (2 + x[2]) ^ 2));
+        }
+
+        private static void ArgumentExceptions(int first, int second, int third)
+        {
+            if (first <= 0 || second <= 0 || third <= 0)
+            {
+                throw new ArgumentException("Input value(s) cannot be null");
+            }
         }
     }
 }
