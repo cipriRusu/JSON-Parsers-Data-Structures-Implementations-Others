ed816e090b1e1b9685e73b339631841b33ad27d4
Cipri Rusu <cipri91rusu@gmail.com>
2020-05-20 06:51:06 -0700

    PythaghoreicTriple Refactor

--- Functional-LINQ/PythagorheicTriple/PythagorheicTriple.cs
+++ Functional-LINQ/PythagorheicTriple/PythagorheicTriple.cs
@@ -15,18 +15,19 @@ namespace Functional_LINQ.PythagorheicTriple
         {
             var sortedInput = input.OrderByDescending(x => x).ToHashSet();
 
-            PytagorheicTriples = sortedInput.Select(x =>
-            new
-            {
-                r = x,
-                s = sortedInput.Where(a => a < x)
+            PytagorheicTriples =
+                sortedInput
+                .Select(x => new
+                {
+                    r = x,
+                    s = sortedInput.Where(a => a < x)
                 .Select(y => new
                 {
                     t = y,
                     q = sortedInput.SkipWhile(x => x >= y)
                     .Where(a => Math.Pow(y, 2) + Math.Pow(a, 2) == Math.Pow(x, 2))
                 }).Where(x => x.q.Count() > 0)
-            }).Where(x => x.s.Count() > 0)
+                }).Where(x => x.s.Count() > 0)
             .ToDictionary(x => x.r, x => new int[]
             {
                 x.s.First().t,
