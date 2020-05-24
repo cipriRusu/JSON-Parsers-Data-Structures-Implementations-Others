95b024bef9a5b52d384a32223dd3dab9f065b830
Cipri Rusu <cipri91rusu@gmail.com>
2020-05-24 07:01:53 -0700

    Rewritten Pythagorhic Triples (UnitTests/Base Implementation)

---
+++ Functional-LINQ/PythagorhicTriple/PythaghoricTripleTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Functional_LINQ.PythagorheicTriple
+{
+    public class PythaghoricTripleTest
+    {
+        [Fact]
+        public void PythagorhicTriplesThrowsArgumentNullExceptionForNullInput()
+        {
+            var triples = new PythagorhicTriple();
+
+            Assert.Throws<ArgumentNullException>(() => triples.GetTriple(null));
+        }
+
+        [Fact]
+        public void PythagorhicTriplesReturnsNoValuesForNoPresentValuesInArray()
+        {
+            var triples = new PythagorhicTriple();
+
+            var output = new (int a, int b, int c)[] {};
+
+            Assert.Equal(output, triples.GetTriple(new int[] { 1, 2, 3 }));
+        }
+
+        [Fact]
+        public void PythagorhicTriplesReturnsOutputForSinglePythahoricArray()
+        {
+            var triples = new PythagorhicTriple();
+
+            var output = new (int a, int b, int c)[] { };
+
+            Assert.Equal(output, triples.GetTriple(new int[] { 3, 4, 5 }));
+        }
+    }
+}
---
+++ Functional-LINQ/PythagorhicTriple/PythagorhicTriple.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Functional_LINQ.PythagorheicTriple
+{
+    internal class PythagorhicTriple
+    {
+        internal IEnumerable<(int a, int b, int c)> GetTriple(int[] input)
+        {
+            var triplets = input.SelectMany((a, b) => input.Skip(b + 1).SelectMany((n, t) =>
+            input.SkipWhile(q => q + 1 > n).Select(j => (a, n, j))));
+
+            return triplets.Where(x => Math.Pow(x.a, 2) == Math.Pow(x.n, 2) + Math.Pow(x.j, 2));
+        }
+    }
+}
