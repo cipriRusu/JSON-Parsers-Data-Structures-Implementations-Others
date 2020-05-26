771ec4afd8fef5922e3a13d810df7765d21473c1
Cipri Rusu <cipri91rusu@gmail.com>
2020-05-26 14:57:27 +0300

    Implemented NoFeatures Filter & Unit Tests / Included Equality to Features collection

--- Functional-LINQ/ProductQueries/Product.cs
+++ Functional-LINQ/ProductQueries/Product.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -12,27 +10,8 @@ namespace Functional_LINQ.ProductQueries
     {
         public string Name { get; set; }
         public ICollection<Feature> Features { get; set; }
-
-        public bool Equals([AllowNull] Product other) => Name == other.Name;
-    }
-
-    public class ProductFilter
-    {
-        private List<Product> _products;
-
-        public ProductFilter(List<Product> products)
-        {
-            _products = products;
-        }
-
-        internal IEnumerable<Product> ProductWithLeastOneFeature(List<Feature> featuresList)
-        {
-            return _products.Where(x => x.Features.Any(y => featuresList.Contains(y)));
-        }
-
-        internal IEnumerable ProductWithAllFeatures(List<Feature> featuresList)
-        {
-            return _products.Where(x => x.Features.All(y => featuresList.Contains(y)));
-        }
+        public bool Equals([AllowNull] Product other) => 
+            Name == other.Name && 
+            other.Features.All(x => Features.Contains(x));
     }
 }
