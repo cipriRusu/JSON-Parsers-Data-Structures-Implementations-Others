a1da01151cc9d2c636eca4a6461bd7861dbef4d9
Cipri Rusu <cipri91rusu@gmail.com>
2020-05-28 15:23:02 +0300

    Implemented TestsResultsPicker + UnitTests

---
+++ Functional-LINQ/TestResultsPicker/TestResultsPicker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional_LINQ.TestResultsPicker
+{
+    public class TestResultsPicker
+    {
+
+    }
+}
---
+++ Functional-LINQ/TestResultsPicker/TestResultsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Functional_LINQ.TestResultsPicker
+{
+    public class TestResultsTest
+    {
+        [Fact]
+        public void TestResultsReturnsOnlyResultPresentInList()
+        {
+            var testResults = new TestResults();
+        }
+    }
+}
