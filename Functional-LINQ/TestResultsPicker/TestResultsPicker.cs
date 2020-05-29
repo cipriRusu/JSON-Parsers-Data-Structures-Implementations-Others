af05695a9f380184fdf9db2c31278b8b71be4a63
Cipri Rusu <cipri91rusu@gmail.com>
2020-05-29 17:28:41 +0300

    TestResults Implementation + Unit Tests (Valid Commit)

--- Functional-LINQ/TestResultsPicker/TestResultsPicker.cs
+++ Functional-LINQ/TestResultsPicker/TestResultsPicker.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Functional_LINQ.TestResultsPicker
 {
     public class TestResultsPicker
     {
-
+        internal IEnumerable<TestResults> MaximumScorePicker(List<TestResults> inputResults)
+        {
+            return inputResults
+                .GroupBy(result => result.FamilyId)
+                    .Select(resultGrouping => resultGrouping.OrderByDescending(n => n.Score))
+                .Select(y => y.TakeWhile(n => n.Score == y.First().Score))
+                .SelectMany(i => i);
+        }
     }
 }
