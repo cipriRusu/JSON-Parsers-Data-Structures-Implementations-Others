414d6ef43f1185e42a32e0d9efdef9a290fa69b2
Cipri Rusu <cipri91rusu@gmail.com>
2020-06-02 12:30:34 +0300

    TestResults refactoring

--- Functional-LINQ/TestResultsPicker/TestResultsPicker.cs
+++ Functional-LINQ/TestResultsPicker/TestResultsPicker.cs
@@ -7,13 +7,7 @@ namespace Functional_LINQ.TestResultsPicker
     public class TestResultsPicker
     {
         internal IEnumerable<TestResults> MaximumScorePicker(List<TestResults> inputResults)
-        {
-            //return inputResults.GroupBy(x => x.FamilyId).Select(y => y.OrderByDescending(z => z.Score).First());
-
-            return inputResults.GroupBy(x => x.FamilyId).Select(y =>
-            {
-                return y.Aggregate(y.First(), (a, b) => b.Score > y.First().Score ? b : a);
-            });
-        }
+            => inputResults.GroupBy(x => x.FamilyId).Select(y => 
+            { return y.Aggregate(y.First(), (a, b) => b.Score > y.First().Score ? b : a); });
     }
 }
