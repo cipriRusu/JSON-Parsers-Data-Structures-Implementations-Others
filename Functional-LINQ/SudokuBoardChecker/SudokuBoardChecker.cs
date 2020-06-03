ce3825457b874823fbd190c40c7239fed9b4349c
Cipri Rusu <cipri91rusu@gmail.com>
2020-06-03 19:32:15 +0300

    Implemented SudokuBoard validator + UnitTests

---
+++ Functional-LINQ/SudokuBoardChecker/SudokuBoardChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Xunit.Sdk;
+
+namespace Functional_LINQ.SudokuBoardChecker
+{
+    public class SudokuBoardChecker
+    {
+        public bool SudokuValidator(int[,] inputMatrix) => 
+            MatrixChecker(inputMatrix, true) && 
+            MatrixChecker(inputMatrix, false);
+
+        private static bool MatrixChecker(int[,] inputMatrix, bool checkColumn)
+        {
+            var indexes = Enumerable.Range(0, inputMatrix.GetLength(0));
+
+            return indexes
+                .Select(x => indexes
+                    .Select(y => checkColumn ? inputMatrix[y, x] : inputMatrix[x, y])
+                        .ContainsAllDigits())
+                .All(y => y == true);
+        }
+    }
+}
