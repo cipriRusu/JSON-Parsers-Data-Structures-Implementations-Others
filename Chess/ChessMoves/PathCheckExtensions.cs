1461004b5ae11d80fa3013c45c9ec593ea8a4258
Cipri Rusu <cipri91rusu@gmail.com>
2020-06-29 11:07:43 +0300

    Refactored PathCheckExtensions

--- Chess/ChessMoves/PathCheckExtensions.cs
+++ Chess/ChessMoves/PathCheckExtensions.cs
@@ -18,45 +18,40 @@ namespace ChessMoves
                 input.Skip(1).SkipLast(1).All(x => board[x.Item1, x.Item2] == null);
         }
 
-        public static bool IsOpponentPathClear(this IEnumerable<(int, int)> input, Player player, Piece[,] board, bool isKnight)
+        public static bool IsOpponentPathClear(
+            this IEnumerable<(int, int)> input, Player player, Piece[,] board, bool isKnight)
         {
-            if (player == Player.White)
+            switch (player)
             {
-                if (isKnight == false)
-                {
-                    return
-                        input.IsCapturePathClear(board) && 
+                case Player.White:
+                    return isKnight == false ? IsOpponent(input, player, board, Player.Black) : 
+                        IsOpponentKnight(input, board, Player.Black);
+
+                case Player.Black:
+                    return isKnight == false ? IsOpponent(input, player, board, Player.White) : 
+                        IsOpponentKnight(input, board, Player.White);
+
+                default:
+                    return false;
+            }
+
+            static bool IsOpponentKnight(
+                IEnumerable<(int, int)> input, Piece[,] board, Player opponentPlayer)
+            {
+                return
+                    board[input.Single().Item1, input.Single().Item2] != null &&
+                    board[input.Single().Item1, input.Single().Item2].PlayerColour == opponentPlayer;
+            }
+
+            static bool IsOpponent(
+                IEnumerable<(int, int)> input, Player player, Piece[,] board, Player opponentPlayer)
+            {
+                return
+                    input.IsCapturePathClear(board) &&
                     board[input.First().Item1, input.First().Item2] != null &&
                     board[input.First().Item1, input.First().Item2].PlayerColour == player &&
-                    board[input.Last().Item1, input.Last().Item2].PlayerColour == Player.Black;
-                }
-                else
-                {
-                    return 
-                    board[input.Single().Item1, input.Single().Item2] != null &&
-                    board[input.Single().Item1, input.Single().Item2].PlayerColour == Player.Black;
-                }
+                    board[input.Last().Item1, input.Last().Item2].PlayerColour == opponentPlayer;
             }
-
-            if (player == Player.Black)
-            {
-                if (isKnight == false)
-                {
-                    return
-                        input.IsCapturePathClear(board) &&
-                board[input.First().Item1, input.First().Item2] != null &&
-                board[input.First().Item1, input.First().Item2].PlayerColour == player &&
-                board[input.Last().Item1, input.Last().Item2].PlayerColour == Player.White;
-                }
-                else
-                {
-                    return
-                    board[input.Single().Item1, input.Single().Item2] != null &&
-                    board[input.Single().Item1, input.Single().Item2].PlayerColour == Player.White;
-                }
-            }
-
-            return false;
         }
     }
 }
