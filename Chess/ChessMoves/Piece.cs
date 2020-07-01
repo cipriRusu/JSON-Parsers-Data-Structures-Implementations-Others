8e2da5ff8845346014b359ab3b4911d41b441bc2
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-01 11:44:55 +0300

    Moved path methods in Piece Encapsulated path validation (ValidatePath method) Added IsChecked() public member Implemented virtual Move() method

--- Chess/ChessMoves/Piece.cs
+++ Chess/ChessMoves/Piece.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 
 namespace ChessMoves
 {
@@ -14,9 +15,7 @@ namespace ChessMoves
         public PieceType PieceType { get; internal set; }
         public virtual IEnumerable<IEnumerable<(int, int)>> GetLegalMoves() => null;
         public bool CheckIndexes(int x, int y) => (x >= 0 && x <= 7) && (y >= 0 && y <= 7);
-
         internal Index customIndex = new Index();
-
         public Piece(string chessBoardIndex, Player playerColour)
         {
             CurrentPosition = customIndex.GetMatrixIndex(chessBoardIndex);
@@ -24,7 +23,6 @@ namespace ChessMoves
             File = chessBoardIndex.First();
             Rank = chessBoardIndex.Last();
         }
-
         public Piece((int, int) pieceIndex, Player playerColour)
         {
             var rankAndFile = new RankAndFile(pieceIndex);
@@ -33,8 +31,7 @@ namespace ChessMoves
             File = rankAndFile.File;
             Rank = rankAndFile.Rank;
         }
-
-        public void UpdatePosition((int, int) newPosition)
+        public void Update((int, int) newPosition)
         {
             var rankAndFile = new RankAndFile(newPosition);
             CurrentPosition = newPosition;
@@ -42,24 +39,158 @@ namespace ChessMoves
             Rank = rankAndFile.Rank;
         }
 
-        internal virtual bool IsChecked(Piece[,] board) => false;
-        internal virtual Piece[,] Move(UserMove move, Piece[,] board) => Movement(move, board);
-        internal virtual Piece[,] MoveTo((int, int) input, Piece[,] board) => SwapPiecesAndUpdate(input, board);
-        internal virtual bool IsCheckMate(Piece[,] chessBoard) => false;
-        internal virtual IEnumerable<IEnumerable<(int, int)>> PawnCapture() => null;
-
-        private Piece[,] SwapPiecesAndUpdate((int, int) input, Piece[,] board)
+        protected IEnumerable<IEnumerable<(int, int)>> ValidatePath(UserMove move)
         {
-            board[input.Item1, input.Item2] = board[CurrentPosition.Item1, CurrentPosition.Item2];
-            board[CurrentPosition.Item1, CurrentPosition.Item2] = null;
-            board[input.Item1, input.Item2].UpdatePosition(input);
+            if (move.UserMoveType == UserMoveType.Move)
+            {
+                if (move.PieceType == PieceType.Knight)
+                {
+                    return GetLegalMoves()
+                    .Where(x => x.Single() == move.MoveIndex);
+                }
+                else
+                {
+                    return GetLegalMoves()
+                    .Where(x => x.First() == CurrentPosition &&
+                                x.Last() == move.MoveIndex);
+                }
+            }
 
-            return board;
+            if (move.UserMoveType == UserMoveType.Capture)
+            {
+                if (move.PieceType == PieceType.Knight)
+                {
+                    return GetLegalMoves()
+                    .Where(x => x.Single() == move.MoveIndex);
+                }
+                else if(move.PieceType == PieceType.Pawn)
+                {
+                    return PawnCapture()
+                    .Where(x => x.Single() == move.MoveIndex);
+                }
+            }
+
+            return null;
         }
 
-        private Piece[,] Movement(UserMove move, Piece[,] board) => GetLegalMoves()
-                .Where(x => x.Last() == move.MoveIndex &&
-                x.CheckPath(board, move.UserMoveType)).Count() > 0 ?
-                SwapPiecesAndUpdate(move.MoveIndex, board) : board;
+        internal bool IsChecked(ChessBoard chessBoard)
+        {
+            return false;
+        }
+
+        protected IEnumerable<IEnumerable<(int, int)>> RowsAndColumns()
+        {
+            var firstColumn = new List<(int, int)>();
+            var firstRow = new List<(int, int)>();
+            var secondColumn = new List<(int, int)>();
+            var secondRow = new List<(int, int)>();
+
+            for (int i = CurrentPosition.Item1; i >= 0; i--)
+            {
+                firstColumn.Add((i, CurrentPosition.Item2));
+            }
+
+            for (int i = CurrentPosition.Item2; i <= 7; i++)
+            {
+                firstRow.Add((CurrentPosition.Item1, i));
+            }
+
+            for (int i = CurrentPosition.Item1; i <= 7; i++)
+            {
+                secondColumn.Add((i, CurrentPosition.Item2));
+            }
+
+            for (int i = CurrentPosition.Item2; i >= 0; i--)
+            {
+                secondRow.Add((CurrentPosition.Item1, i));
+            }
+
+            var firstSubs = firstColumn.Select((x, y) => firstColumn.Take(y + 1)).Skip(1);
+            var firstRowSubs = firstRow.Select((x, y) => firstRow.Take(y + 1)).Skip(1);
+            var secondSubs = secondColumn.Select((x, y) => secondColumn.Take(y + 1)).Skip(1);
+            var secondColSubs = secondRow.Select((x, y) => secondRow.Take(y + 1)).Skip(1);
+
+            return firstSubs.Concat(firstRowSubs).Concat(secondSubs).Concat(secondColSubs);
+        }
+
+        protected IEnumerable<IEnumerable<(int, int)>> Diagonals()
+        {
+            var firstDiag = new List<(int, int)>();
+            var secondDiag = new List<(int, int)>();
+            var thirdDiag = new List<(int, int)>();
+            var fourthDiag = new List<(int, int)>();
+
+            for (int i = CurrentPosition.Item1, j = CurrentPosition.Item2;
+                i >= 0 && j >= 0; i--, j--)
+            {
+                firstDiag.Add((i, j));
+            }
+
+            for (int i = CurrentPosition.Item1, j = CurrentPosition.Item2;
+                i >= 0 && j <= 7; i--, j++)
+            {
+                secondDiag.Add((i, j));
+            }
+
+            for (int i = CurrentPosition.Item1, j = CurrentPosition.Item2;
+               i <= 7 && j >= 0; i++, j--)
+            {
+                thirdDiag.Add((i, j));
+            }
+
+            for (int i = CurrentPosition.Item1, j = CurrentPosition.Item2;
+               i <= 7 && j <= 7; i++, j++)
+            {
+                fourthDiag.Add((i, j));
+            }
+
+            var firstSubArrays = firstDiag.Select((x, y) => firstDiag.Take(y + 1)).Skip(1);
+            var secondSubArrays = secondDiag.Select((x, y) => secondDiag.Take(y + 1)).Skip(1);
+            var thirdSubArrays = thirdDiag.Select((x, y) => thirdDiag.Take(y + 1)).Skip(1);
+            var fourthSubArrays = fourthDiag.Select((x, y) => fourthDiag.Take(y + 1)).Skip(1);
+
+            return firstSubArrays.Concat(secondSubArrays).Concat(thirdSubArrays).Concat(fourthSubArrays);
+        }
+
+        protected IEnumerable<IEnumerable<(int, int)>> PawnCapture()
+        {
+            var captures = new List<IEnumerable<(int, int)>>();
+
+            if (PlayerColour == Player.White)
+            {
+                if (CheckIndexes(CurrentPosition.Item1 - 1, CurrentPosition.Item2 + 1))
+                {
+                    captures.Add(Enumerable.Repeat((CurrentPosition.Item1 - 1, CurrentPosition.Item2 + 1), 1));
+                }
+                if (CheckIndexes(CurrentPosition.Item1 - 1, CurrentPosition.Item2 - 1))
+                {
+                    captures.Add(Enumerable.Repeat((CurrentPosition.Item1 - 1, CurrentPosition.Item2 - 1), 1));
+                }
+            }
+            else if (PlayerColour == Player.Black)
+            {
+                if (CheckIndexes(CurrentPosition.Item1 + 1, CurrentPosition.Item2 - 1))
+                {
+                    captures.Add(Enumerable.Repeat((CurrentPosition.Item1 + 1, CurrentPosition.Item2 - 1), 1));
+                }
+                if (CheckIndexes(CurrentPosition.Item1 + 1, CurrentPosition.Item2 + 1))
+                {
+                    captures.Add(Enumerable.Repeat((CurrentPosition.Item1 + 1, CurrentPosition.Item2 + 1), 1));
+                }
+            }
+
+            return captures;
+        }
+
+        internal virtual void Move(UserMove move, ChessBoard chessBoard)
+        {
+            var validPath = ValidatePath(move).SelectMany(x => x);
+
+            if (validPath.Any() && chessBoard.IsPathClear(validPath.Skip(1)))
+            {
+                chessBoard
+                    .PerformMove(validPath.First(), validPath.Last());
+            }
+        }
     }
 }
