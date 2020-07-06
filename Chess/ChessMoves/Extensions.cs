cfd837615f42c68981d01d601aef43ebe257c496
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-06 14:59:24 +0300

    Implemented extension method for DeepCloning

---
+++ Chess/ChessMoves/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace ChessMoves
+{
+    public static class Extensions
+    {
+        public static T DeepClone<T>(this T toClone)
+        {
+            using var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, toClone);
+            stream.Seek(0, SeekOrigin.Begin);
+            return (T)formatter.Deserialize(stream);
+        }
+    }
+}
