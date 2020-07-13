4bfd507b99aa4e12c02e28d0c6da139b8333f79b
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-13 17:36:41 +0300

    Removed MoveHandler class

--- Chess/ChessMoves/ChessBoard.cs
+++ Chess/ChessMoves/ChessBoard.cs
@@ -29,9 +29,24 @@ namespace ChessMoves
 
         private void Move(UserMove move)
         {
-            var handledPiece = new MoveHandler(move, this).GetHandledPiece();
-            PieceNotFoundException(handledPiece);
-            PerformMove(handledPiece.CurrentPosition, move.MoveIndex);
+            foreach (var i in Enumerable.Range(0, CHESSBOARD_SIZE))
+            {
+                foreach (var j in Enumerable.Range(0, CHESSBOARD_SIZE))
+                {
+                    if (PieceConstraint(move, i, j) &&
+                        (RankConstraint(move, i, j) ||
+                        FileConstraint(move, i, j) ||
+                        FileAndRankConstraint(move, i, j) ||
+                        NoConstraint(move)))
+                    {
+                        if (board[i, j].IsMoveValid(this, move))
+                        {
+                            PieceNotFoundException(board[i, j]);
+                            PerformMove(board[i, j].CurrentPosition, move.MoveIndex);
+                        }
+                    }
+                }
+            }
         }
 
         internal bool IsPiece((int, int) currentPosition, PieceType pieceType, Player player)
@@ -61,6 +76,29 @@ namespace ChessMoves
             board[source.Item1, source.Item2] = null;
 
             SwitchTurn();
+        }
+
+        private bool NoConstraint(UserMove move) =>
+            move.SourceFile == '\0' && move.SourceRank == '\0';
+
+        private bool FileConstraint(UserMove move, int i, int j) =>
+            move.SourceFile != '\0' && move.SourceRank == '\0' && move.SourceFile == board[i, j].File;
+
+        private bool RankConstraint(UserMove move, int i, int j) =>
+            move.SourceRank != '\0' && move.SourceFile == '\0' && move.SourceRank == board[i, j].Rank;
+
+        private bool FileAndRankConstraint(UserMove move, int i, int j) =>
+            move.SourceRank != '\0' &&
+            move.SourceFile != '\0' &&
+            move.SourceRank == board[i, j].Rank &&
+            move.SourceFile == board[i, j].File;
+
+        private bool PieceConstraint(UserMove move, int i, int j)
+        {
+            return
+                board[i, j] != null &&
+                board[i, j].PlayerColour == move.PlayerColor &&
+                board[i, j].PieceType == move.PieceType;
         }
 
         private IEnumerable<UserMove> ConvertToUserMoves(IEnumerable<string> input)
