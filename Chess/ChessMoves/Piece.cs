618cf8761e849211f63ea352f1d4c557a0eb8055
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-13 15:48:03 +0300

    - Removed path methods - Modified virtual method for Path.cs type - Extracted helper method for constructors

--- Chess/ChessMoves/Piece.cs
+++ Chess/ChessMoves/Piece.cs
@@ -9,13 +9,14 @@ namespace ChessMoves
     [Serializable]
     public class Piece
     {
+        public Piece() { }
         public const int BOARDSIZE = 8;
         public (int, int) CurrentPosition { get; internal set; }
         public Player PlayerColour { get; internal set; }
         public char File { get; private set; }
         public char Rank { get; private set; }
         public PieceType PieceType { get; internal set; }
-        public virtual IEnumerable<IEnumerable<(int, int)>> GetLegalMoves() => null;
+        public virtual Path GetLegalMoves() => null;
         public bool CheckIndexes(int x, int y) => (x >= 0 && x <= 7) && (y >= 0 && y <= 7);
         internal Index customIndex = new Index();
         public Piece(string chessBoardIndex, Player playerColour)
@@ -25,20 +26,14 @@ namespace ChessMoves
             File = chessBoardIndex.First();
             Rank = chessBoardIndex.Last();
         }
-        public Piece((int, int) pieceIndex, Player playerColour)
+        public Piece((int, int) position, Player playerColour)
         {
-            var rankAndFile = new RankAndFile(pieceIndex);
-            CurrentPosition = pieceIndex;
+            InitializePiece(position);
             PlayerColour = playerColour;
-            File = rankAndFile.File;
-            Rank = rankAndFile.Rank;
         }
         public void Update((int, int) newPosition)
         {
-            var rankAndFile = new RankAndFile(newPosition);
-            CurrentPosition = newPosition;
-            File = rankAndFile.File;
-            Rank = rankAndFile.Rank;
+            InitializePiece(newPosition);
         }
 
         public static Player Opponent(Player player)
@@ -68,95 +63,26 @@ namespace ChessMoves
             }
         }
 
-        private bool IsOpponentColour(ChessBoard board, (int, int) lastElement) =>
-            board[lastElement.Item1, lastElement.Item2].PlayerColour == Opponent(PlayerColour);
+        internal virtual bool IsChecked(ChessBoard board, Player player) => false;
+
+        internal virtual bool IsMoveValid(ChessBoard board, UserMove move)
+        {
+            var currentPath = ValidatePath(board, move).SelectMany(x => x);
+
+            return currentPath.Any() && board.IsPathClear(currentPath.Skip(1).SkipLast(1));
+        }
+
+        private bool IsOpponentColour(ChessBoard board, (int, int) expectedOpponent) =>
+            board[expectedOpponent.Item1, expectedOpponent.Item2].PlayerColour == Opponent(PlayerColour);
 
         private bool IsLast((int, int) actualLast, (int, int) expectedLast) => actualLast == expectedLast;
-        internal virtual bool IsChecked(Player player, ChessBoard chessBoard) => false;
-        internal virtual bool IsCheckMated(Player player, ChessBoard chessBoard) => false;
 
-        protected IEnumerable<IEnumerable<(int, int)>> RowsAndColumns()
+        private void InitializePiece((int, int) position)
         {
-            var firstColumn = new List<(int, int)>();
-            var firstRow = new List<(int, int)>();
-            var secondColumn = new List<(int, int)>();
-            var secondRow = new List<(int, int)>();
-
-            for (int i = CurrentPosition.Item1; i >= 0; i--)
-            {
-                firstColumn.Add((i, CurrentPosition.Item2));
-            }
-
-            for (int i = CurrentPosition.Item2; i <= BOARDSIZE - 1; i++)
-            {
-                firstRow.Add((CurrentPosition.Item1, i));
-            }
-
-            for (int i = CurrentPosition.Item1; i <= BOARDSIZE - 1; i++)
-            {
-                secondColumn.Add((i, CurrentPosition.Item2));
-            }
-
-            for (int i = CurrentPosition.Item2; i >= 0; i--)
-            {
-                secondRow.Add((CurrentPosition.Item1, i));
-            }
-
-            var firstSubs = firstColumn.Select((x, y) => firstColumn.Take(y + 1)).Skip(1);
-            var firstRowSubs = firstRow.Select((x, y) => firstRow.Take(y + 1)).Skip(1);
-            var secondSubs = secondColumn.Select((x, y) => secondColumn.Take(y + 1)).Skip(1);
-            var secondColSubs = secondRow.Select((x, y) => secondRow.Take(y + 1)).Skip(1);
-
-            return firstSubs.Concat(firstRowSubs).Concat(secondSubs).Concat(secondColSubs);
-        }
-
-        protected IEnumerable<IEnumerable<(int, int)>> Diagonals()
-        {
-            var firstDiag = new List<(int, int)>();
-            var secondDiag = new List<(int, int)>();
-            var thirdDiag = new List<(int, int)>();
-            var fourthDiag = new List<(int, int)>();
-
-            for (int i = CurrentPosition.Item1, j = CurrentPosition.Item2;
-                i >= 0 && j >= 0; i--, j--)
-            {
-                firstDiag.Add((i, j));
-            }
-
-            for (int i = CurrentPosition.Item1, j = CurrentPosition.Item2;
-                i >= 0 && j <= 7; i--, j++)
-            {
-                secondDiag.Add((i, j));
-            }
-
-            for (int i = CurrentPosition.Item1, j = CurrentPosition.Item2;
-               i <= 7 && j >= 0; i++, j--)
-            {
-                thirdDiag.Add((i, j));
-            }
-
-            for (int i = CurrentPosition.Item1, j = CurrentPosition.Item2;
-               i <= 7 && j <= 7; i++, j++)
-            {
-                fourthDiag.Add((i, j));
-            }
-
-            var firstSubArrays = firstDiag.Select((x, y) => firstDiag.Take(y + 1)).Skip(1);
-            var secondSubArrays = secondDiag.Select((x, y) => secondDiag.Take(y + 1)).Skip(1);
-            var thirdSubArrays = thirdDiag.Select((x, y) => thirdDiag.Take(y + 1)).Skip(1);
-            var fourthSubArrays = fourthDiag.Select((x, y) => fourthDiag.Take(y + 1)).Skip(1);
-
-            return firstSubArrays.Concat(secondSubArrays).Concat(thirdSubArrays).Concat(fourthSubArrays);
-        }
-
-        internal virtual void Move(UserMove move, ChessBoard chessBoard)
-        {
-            var validPath = ValidatePath(chessBoard, move).SelectMany(x => x);
-
-            if (validPath.Any() && chessBoard.IsPathClear(validPath.Skip(1).SkipLast(1)))
-            {
-                chessBoard.PerformMove(validPath.First(), validPath.Last());
-            }
+            var rankAndFile = new RankAndFile(position);
+            CurrentPosition = position;
+            File = rankAndFile.File;
+            Rank = rankAndFile.Rank;
         }
     }
 }
