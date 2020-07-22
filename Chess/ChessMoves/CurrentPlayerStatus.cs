4eb7e7184778ebf3c60ae74eda77691b2877bc83
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-22 12:58:16 +0300

    Refactor : implemented currentKing as private property Implemented Castling validation methods

--- Chess/ChessMoves/CurrentPlayerStatus.cs
+++ Chess/ChessMoves/CurrentPlayerStatus.cs
@@ -9,6 +9,11 @@ namespace ChessMoves
     {
         private Player turnToMove;
         private ChessBoard chessBoard;
+        private Piece _currentKing
+        {
+            get => FindKing();
+            set => _currentKing = value;
+        }
 
         public CurrentPlayerStatus(Player turnToMove, ChessBoard chessBoard)
         {
@@ -17,7 +22,6 @@ namespace ChessMoves
         }
 
         public bool IsChecked => KingCheckStatus();
-
         public bool IsCheckMated => KingCheckMateStatus();
 
         public void GetCurrentState(Player turnToMove, ChessBoard chessBoard)
@@ -28,26 +32,24 @@ namespace ChessMoves
 
         private bool KingCheckStatus()
         {
-            Piece currentKing = FindKing();
-
             IEnumerable<IEnumerable<(int, int)>> diagonalAttacks =
-                GetAttacks(currentKing,
+                GetAttacks(_currentKing,
                 new PathType[] { PathType.Diagonals },
                 new PieceType[] { PieceType.Queen, PieceType.Bishop });
 
             IEnumerable<IEnumerable<(int, int)>> verticalHorizontalAttacks =
-                GetAttacks(currentKing,
+                GetAttacks(_currentKing,
                 new PathType[] { PathType.RowsAndColumns },
                 new PieceType[] { PieceType.Queen, PieceType.Rock });
 
             IEnumerable<IEnumerable<(int, int)>> knightAttacks =
-                GetAttacks(currentKing,
+                GetAttacks(_currentKing,
                 new PathType[] { PathType.Knight },
                 new PieceType[] { PieceType.Knight });
 
             IEnumerable<IEnumerable<(int, int)>> pawnAttacks =
-                GetAttacks(currentKing, 
-                new PathType[] { PathType.PawnCapture }, 
+                GetAttacks(_currentKing,
+                new PathType[] { PathType.PawnCapture },
                 new PieceType[] { PieceType.Pawn });
 
             return diagonalAttacks.Any() || verticalHorizontalAttacks.Any() || knightAttacks.Any() || pawnAttacks.Any();
@@ -57,10 +59,43 @@ namespace ChessMoves
         {
             return new Path(currentKing.CurrentPosition, pathTypes)
                 .Where(x =>
-                chessBoard[x.Last()] != null &&
-                chessBoard.IsPathClear(x.Skip(1).SkipLast(1)) &&
-                chessBoard[x.Last()].PlayerColour == Piece.Opponent(turnToMove) &&
-                attackers.Contains(chessBoard[x.Last()].PieceType));
+                chessBoard[x.Last()] != null
+                && chessBoard.IsPathClear(x.Skip(1).SkipLast(1))
+                && chessBoard[x.Last()].PlayerColour == Piece.Opponent(turnToMove)
+                && attackers.Contains(chessBoard[x.Last()].PieceType));
+        }
+
+        internal bool IsCastlingValid(UserMove move, IEnumerable<(int, int)> castlingPath)
+        {
+            switch (move.UserMoveType)
+            {
+                case UserMoveType.KingCastling:
+                    return CastlingValidator(castlingPath, 1);
+                case UserMoveType.QueenCastling:
+                    return CastlingValidator(castlingPath, 2);
+                default:
+                    return true;
+            }
+        }
+
+        private bool CastlingValidator(IEnumerable<(int, int)> castlingPath, int edgeSkip) => 
+            KingCastlingValidation(castlingPath) && 
+            castlingPath.Skip(edgeSkip)
+                        .SkipLast(1)
+                        .Select(x => !KingAttackStatus(x))
+                        .All(x => true);
+
+        private bool KingCastlingValidation(IEnumerable<(int, int)> castlingPath) =>
+            !KingCheckStatus()
+            && chessBoard.IsPathClear(castlingPath.Skip(1).SkipLast(1));
+
+        private bool KingAttackStatus((int, int) targetPosition)
+        {
+            var currentBoardState = chessBoard.DeepClone();
+
+            currentBoardState.PerformMove(FindKing().CurrentPosition, targetPosition);
+
+            return new CurrentPlayerStatus(turnToMove, currentBoardState).IsChecked;
         }
 
         private bool KingCheckMateStatus()
@@ -83,8 +118,9 @@ namespace ChessMoves
         }
 
         private Piece FindKing() =>
-            chessBoard.GetAllPieces().Where(x => x != null &&
-            x.PieceType == PieceType.King &&
-            x.PlayerColour == turnToMove).Single();
+            chessBoard.GetAllPieces().Where(
+            x => x != null
+                 && x.PieceType == PieceType.King
+                 && x.PlayerColour == turnToMove).Single();
     }
 }
