355e66ae23b24f4bb49ed9e79fa9a2b1f6a1496e
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-24 15:49:39 +0300

    Added EnPassant class for handling EnPassant special move

---
+++ Chess/ChessMoves/EnPassant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ChessMoves
+{
+    internal class EnPassant
+    {
+        private ChessBoard chessBoard;
+        private UserMove move;
+
+        public EnPassant(ChessBoard chessBoard, UserMove move)
+        {
+            this.chessBoard = chessBoard;
+            this.move = move;
+
+            EnPassantHandler();
+        }
+
+        private void EnPassantHandler()
+        {
+            if (move.PlayerColor == Player.White)
+            {
+                if (chessBoard[move.MoveIndex.Item1 + 1, move.MoveIndex.Item2].PlayerColour == Piece.Opponent(move.PlayerColor)
+                    && chessBoard[move.MoveIndex.Item1 + 1, move.MoveIndex.Item2].IsPassantCapturable)
+                {
+                    chessBoard.Remove((move.MoveIndex.Item1 + 1, move.MoveIndex.Item2));
+                }
+            }
+            if (move.PlayerColor == Player.Black)
+            {
+                if (chessBoard[move.MoveIndex.Item1 - 1, move.MoveIndex.Item2].PlayerColour == Piece.Opponent(move.PlayerColor)
+                    && chessBoard[move.MoveIndex.Item1 - 1, move.MoveIndex.Item2].IsPassantCapturable)
+                {
+                    chessBoard.Remove((move.MoveIndex.Item1 - 1, move.MoveIndex.Item2));
+                }
+            }
+        }
+    }
+}
