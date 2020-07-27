56e2022f533dbd909770f9aea45aefac75e48362
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-27 09:33:50 +0300

    Added customized exception for Piece and UserMove

---
+++ Chess/ChessMoves/PieceException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves
+{
+    public class PieceException : Exception
+    {
+        private UserMove move;
+        private IEnumerable<Piece> targetPiece;
+
+        public PieceException(string message) { }
+
+        public PieceException(UserMove move, IEnumerable<Piece> targetPiece, string message) : base(message)
+        {
+            this.move = move;
+            this.targetPiece = targetPiece;
+        }
+    }
+}
---
+++ Chess/ChessMoves/UserMoveException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChessMoves
+{
+    internal class UserMoveException : Exception
+    {
+        private UserMove move;
+
+        public UserMoveException(string message) { }
+
+        public UserMoveException(UserMove move, string message) : base(message)
+        {
+            this.move = move;
+        }
+    }
+}
