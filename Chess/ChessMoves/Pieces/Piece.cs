91e69eaa215b4deadb073dde7d2392d005144a6c
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-27 18:09:00 +0300

    Moved Piece specific classes to Piece Folder

---
+++ Chess/ChessMoves/Pieces/Piece.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace ChessMoves
+{
+    [Serializable]
+    public abstract class Piece
+    {
+        public Piece(string chessBoardIndex, Player playerColour)
+        {
+            CurrentPosition = matrixIndexConvertor.GetMatrixIndex(chessBoardIndex);
+            PlayerColour = playerColour;
+            File = chessBoardIndex.First();
+            Rank = chessBoardIndex.Last();
+        }
+
+        public const int BOARDSIZE = 8;
+        public (int, int) CurrentPosition { get; internal set; }
+        public Player PlayerColour { get; internal set; }
+        public char File { get; private set; }
+        public char Rank { get; private set; }
+        public PieceType PieceType { get; internal set; }
+        public bool IsMoved { get; internal set; }
+        public bool IsPassantCapturable { get; set; }
+        public virtual Path Moves() => null;
+        public virtual Path Captures() => null;
+
+        private readonly Index matrixIndexConvertor = new Index();
+
+        public void Update((int, int) newPosition)
+        {
+            var rankAndFile = new RankAndFile(newPosition);
+            CurrentPosition = newPosition;
+            File = rankAndFile.File;
+            Rank = rankAndFile.Rank;
+        }
+
+        public static Player Opponent(Player player)
+        {
+            switch (player)
+            {
+                case Player.White:
+                    return Player.Black;
+                case Player.Black:
+                    return Player.White;
+                default:
+                    throw new ArgumentException("Invalid player");
+            }
+        }
+    }
+}
