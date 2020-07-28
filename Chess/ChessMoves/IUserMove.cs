6bcbf5262efdc93f9a8643c5a29381fd2b6c6f02
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-28 09:24:33 +0300

    Created abstraction for UserMove

---
+++ Chess/ChessMoves/IUserMove.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves
+{
+    public interface IUserMove
+    {
+        PieceType PieceType { get; }
+        Player PlayerColor { get; }
+        (int, int) MoveIndex { get; }
+        char SourceFile { get; }
+        char SourceRank { get; }
+        public virtual void Move(ChessBoard board) { }
+    }
+}
