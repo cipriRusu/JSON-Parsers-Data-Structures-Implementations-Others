a89233951bd93292d251e6888e0b4574d509a5a4
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-28 15:04:38 +0300

    Overriden CanCapture/PerformCapture methods for Pawn

--- Chess/ChessMoves/Pieces/Pawn.cs
+++ Chess/ChessMoves/Pieces/Pawn.cs
@@ -12,8 +12,20 @@ namespace ChessMoves
         public Pawn(string chessBoardIndex, Player playerColour) : base(chessBoardIndex, playerColour) => 
             PieceType = PieceType.Pawn;
 
+        public override bool CanCapture((int, int) target) => Captures().Any(x => x.Last() == target);
+
         public override Path Moves() => new Path(CurrentPosition, new PathType[] { PathType.Pawn }, PlayerColour);
 
         public override Path Captures() => new Path(CurrentPosition, new PathType[] { PathType.PawnCapture}, PlayerColour);
+
+        public override void PerformCapture((int, int) targetCapture, ChessBoard chessBoard)
+        {
+            var targetPiece = chessBoard[targetCapture];
+
+            if(Opponent(PlayerColour) == targetPiece.PlayerColour)
+            {
+                chessBoard.PerformMove(this, targetCapture);
+            }
+        }
     }
 }
