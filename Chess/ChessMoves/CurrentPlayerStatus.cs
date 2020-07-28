584a09df8d2b90a321a29edf97ee7ec72f9dde2f
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-28 17:37:57 +0300

    Encapsulated/Implemented CurrentPlayerStatus + UnitTests

---
+++ Chess/ChessMoves/CurrentPlayerStatus.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessMoves
+{
+    public class CurrentPlayerStatus
+    {
+        private readonly Player turnToMove;
+        private readonly ChessBoard chessBoard;
+        private IChessPiece _currentKing;
+        private IChessPiece King => chessBoard.GetAllPieces().Where(x => x != null
+        && x.PieceType == PieceType.King
+        && x.PlayerColour == turnToMove).Single();
+
+        public CurrentPlayerStatus(Player turnToMove, ChessBoard chessBoard)
+        {
+            this.turnToMove = turnToMove;
+            this.chessBoard = chessBoard;
+            _currentKing = King;
+        }
+
+        public bool IsChecked => KingCheckStatus();
+
+        private bool KingCheckStatus()
+        {
+            IEnumerable<IEnumerable<(int, int)>> diagonalAttacks =
+                GetAttacks(_currentKing,
+                new PathType[] { PathType.Diagonals },
+                new PieceType[] { PieceType.Queen, PieceType.Bishop });
+
+            IEnumerable<IEnumerable<(int, int)>> verticalHorizontalAttacks =
+                GetAttacks(_currentKing,
+                new PathType[] { PathType.RowsAndColumns },
+                new PieceType[] { PieceType.Queen, PieceType.Rock });
+
+            IEnumerable<IEnumerable<(int, int)>> knightAttacks =
+                GetAttacks(_currentKing,
+                new PathType[] { PathType.Knight },
+                new PieceType[] { PieceType.Knight });
+
+            IEnumerable<IEnumerable<(int, int)>> pawnAttacks =
+                GetAttacks(_currentKing,
+                new PathType[] { PathType.PawnCapture },
+                new PieceType[] { PieceType.Pawn });
+
+            return diagonalAttacks.Any() || verticalHorizontalAttacks.Any() || knightAttacks.Any() || pawnAttacks.Any();
+        }
+
+        private IEnumerable<IEnumerable<(int, int)>> GetAttacks(IChessPiece currentKing, PathType[] pathTypes, PieceType[] attackers)
+        {
+            return new Path(currentKing.CurrentPosition, pathTypes)
+                .Where(x =>
+                chessBoard[x.Last()] != null
+                && chessBoard.IsPathClear(x.Skip(1).SkipLast(1))
+                && chessBoard[x.Last()].PlayerColour == Piece.Opponent(turnToMove)
+                && attackers.Contains(chessBoard[x.Last()].PieceType));
+        }
+    }
+}
