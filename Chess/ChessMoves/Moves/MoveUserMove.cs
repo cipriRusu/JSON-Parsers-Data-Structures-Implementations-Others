72c6ff36cff74ac5bf7753242501fb5753a4d368
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-28 09:34:04 +0300

    Renamed IUserMove to IMove (unchanged functionality)

--- Chess/ChessMoves/Moves/MoveUserMove.cs
+++ Chess/ChessMoves/Moves/MoveUserMove.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ChessMoves.Moves
 {
-    public class MoveUserMove : UserMove, IMove
+    public class MoveUserMove : UserMove, IUserMove
     {
-        public MoveUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
+        public MoveUserMove(string input, Player playerTurn) : base(input, playerTurn)  { }
     }
 }
