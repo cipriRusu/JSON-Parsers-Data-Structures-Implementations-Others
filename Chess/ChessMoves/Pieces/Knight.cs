ac674e2f71bbe953605a1d8ade5b65f82b2e82ee
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-28 13:22:56 +0300

    Implemented CanReach IChessPiece member for Knight Piece

--- Chess/ChessMoves/Pieces/Knight.cs
+++ Chess/ChessMoves/Pieces/Knight.cs
@@ -9,10 +9,17 @@ namespace ChessMoves
     internal class Knight : Piece, IChessPiece
     {
         public Knight(string chessBoardIndex, Player playerColour) :
-            base(chessBoardIndex, playerColour) => 
+            base(chessBoardIndex, playerColour) =>
             PieceType = PieceType.Knight;
 
         public Path Moves() => new Path(CurrentPosition, new PathType[] { PathType.Knight });
         public Path Captures() => Moves();
+        public bool CanReach((int, int) destination) => Moves().Any(x => x.Single() == destination);
+        public void PerformMove((int, int) targetMove, ChessBoard chessBoard)
+        {
+            var validPath = Moves().Where(x => x.Last() == targetMove).SelectMany(x => x);
+
+            chessBoard.PerformMove(this, targetMove);
+        }
     }
 }
