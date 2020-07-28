6d56ef4c8f73430255e84c3135cee483b2152d07
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-28 09:24:07 +0300

    Created abstraction for ChessPiece

---
+++ Chess/ChessMoves/IChessPiece.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves
+{
+    public interface IChessPiece
+    {
+        virtual Path Moves() { return null; }
+        virtual Path Captures() { return null; }
+        virtual bool CanReach((int, int) destination) { return false; }
+    }
+}
