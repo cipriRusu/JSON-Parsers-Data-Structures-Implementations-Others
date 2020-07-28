8d28e407d4a4a3423f90210a8326899316640ecf
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-28 09:48:08 +0300

    Extracted Constraint validation to external class (Incomplete)

---
+++ Chess/ChessMoves/ConstraintValidator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves
+{
+    public class ConstraintValidator
+    {
+        public Piece Piece { get; private set; }
+        public IUserMove Move { get; private set; }
+
+        public ConstraintValidator()
+        {
+
+        }
+
+        private bool AllMoveConstraints(IUserMove move, Piece x) =>
+            RankConstraint(move, x) ||
+            FileConstraint(move, x) ||
+            FileAndRankConstraint(move, x) ||
+            NoConstraint(move);
+
+        private bool NoConstraint(IUserMove move) =>
+                move.SourceFile == '\0' &&
+                move.SourceRank == '\0';
+
+        private bool FileAndRankConstraint(IUserMove move, Piece x) =>
+            move.SourceRank != '\0' &&
+            move.SourceFile != '\0' &&
+            move.SourceRank == x.Rank &&
+            move.SourceFile == x.File;
+
+        private bool FileConstraint(IUserMove move, Piece x) =>
+            move.SourceFile != '\0' &&
+            move.SourceRank == '\0' &&
+            move.SourceFile == x.File;
+
+        private bool RankConstraint(IUserMove move, Piece x) =>
+            move.SourceRank != '\0' &&
+            move.SourceFile == '\0' &&
+            move.SourceRank == x.Rank;
+
+    }
+}
