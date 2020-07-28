f1ee4fc20916fb38a3765876a3137929dc3edeae
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-28 14:59:44 +0300

    Implemented Standard CaptureUserMove/Validation&PerformCapture

--- Chess/ChessMoves/Moves/CaptureUserMove.cs
+++ Chess/ChessMoves/Moves/CaptureUserMove.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ChessMoves
@@ -7,5 +8,17 @@ namespace ChessMoves
     public class CaptureUserMove : UserMove, IUserMove
     {
         public CaptureUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
+
+        public void PerformMoveType(ChessBoard board) 
+        {
+            var current = board.GetAllPieces()
+                .Where(x => x != null
+                            && x.PlayerColour == PlayerColor
+                            && x.PieceType == PieceType
+                            && x.CanCapture(MoveIndex)
+                            && new ConstraintValidator(x, this).IsValid);
+
+            current.Single().PerformCapture(MoveIndex, board);
+        }
     }
 }
