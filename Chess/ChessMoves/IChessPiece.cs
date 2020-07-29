1d2c8db13bce3ced6f3f047ec0c4f2cb2ad9f647
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-29 16:11:17 +0300

    Added IsPassantCapturable property and MarkPassant handling method in IChessPiece interface

--- Chess/ChessMoves/IChessPiece.cs
+++ Chess/ChessMoves/IChessPiece.cs
@@ -13,14 +13,14 @@ namespace ChessMoves
         Player PlayerColour { get; }
         PieceType PieceType { get; }
         bool IsMoved { get; }
+        bool IsPassantCapturable { get; }
         Path Moves() { return null; }
         Path Captures() { return null; }
-
+        void MarkPassant(IChessPiece piece, (int, int) destination) { }
         bool CanReach((int, int) destination, ChessBoard chessBoard) =>
             Moves().Any(x => x.Last() == destination && chessBoard.IsPathClear(x.Skip(1)));
         bool CanCapture((int, int) target, ChessBoard chessBoard) =>
             Captures().Any(x => x.Last() == target && chessBoard.IsPathClear(x.Skip(1).SkipLast(1)));
-
         virtual void PerformMove((int, int) targetMove, ChessBoard chessBoard) { }
         virtual void PerformCapture((int, int) targetCapture, ChessBoard chessBoard) { }
         void PerformCastlingMove((int, int) targetMove, ChessBoard chessBoard) => chessBoard.PerformMove(this, targetMove);
