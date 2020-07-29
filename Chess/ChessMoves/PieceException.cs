b588f246466648d74c20ae2c527d883646deb17d
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-29 18:09:05 +0300

    replaced types with interfaces in exception

--- Chess/ChessMoves/PieceException.cs
+++ Chess/ChessMoves/PieceException.cs
@@ -6,12 +6,12 @@ namespace ChessMoves
 {
     public class PieceException : Exception
     {
-        private UserMove move;
-        private IEnumerable<Piece> targetPiece;
+        private IUserMove move;
+        private IEnumerable<IChessPiece> targetPiece;
 
         public PieceException(string message) { }
 
-        public PieceException(UserMove move, IEnumerable<Piece> targetPiece, string message) : base(message)
+        public PieceException(IUserMove move, IEnumerable<IChessPiece> targetPiece, string message) : base(message)
         {
             this.move = move;
             this.targetPiece = targetPiece;
