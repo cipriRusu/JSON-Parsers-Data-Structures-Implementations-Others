8980d60c07aa50d2333a869deaea3a0ea2b6ef6d
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-29 12:15:22 +0300

    Implemented IsMoved member in IChessPiece (for castling move validation) Updated CanReach and CanCapture methods with chessboard parameter Inserted PerformCastlingMove method in IChessPiece as handling method for Castling moves

--- Chess/ChessMoves/IChessPiece.cs
+++ Chess/ChessMoves/IChessPiece.cs
@@ -11,11 +11,13 @@ namespace ChessMoves
         (int, int) CurrentPosition { get; }
         Player PlayerColour { get; }
         PieceType PieceType { get; }
+        bool IsMoved { get; }
         Path Moves() { return null; }
         Path Captures() { return null; }
-        virtual bool CanReach((int, int) destination) { return false; }
-        virtual bool CanCapture((int, int) target) { return false; }
+        virtual bool CanReach((int, int) destination, ChessBoard chessBoard) { return false; }
+        virtual bool CanCapture((int, int) target, ChessBoard chessBoard) { return false; }
         virtual void PerformMove((int, int) targetMove, ChessBoard chessBoard) { }
         virtual void PerformCapture((int, int) targetCapture, ChessBoard chessBoard) { }
+        void PerformCastlingMove((int, int) targetMove, ChessBoard chessBoard) => chessBoard.PerformMove(this, targetMove);
     }
 }
