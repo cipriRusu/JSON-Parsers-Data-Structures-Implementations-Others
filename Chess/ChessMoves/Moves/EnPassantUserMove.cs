e5b81bb3adb1075b0693e03f6d0e2ba26012d8a3
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-29 16:06:11 +0300

    Implemented EnPassant capture UserMove

--- Chess/ChessMoves/Moves/EnPassantUserMove.cs
+++ Chess/ChessMoves/Moves/EnPassantUserMove.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace ChessMoves.Moves
 {
     public class EnPassantUserMove : UserMove, IUserMove
     {
         public EnPassantUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
+
+        public void PerformMoveType(ChessBoard board)
+        {
+            var validPiece = board.GetAllPieces()
+                .Where(
+                x =>
+                x != null &&
+                x.PlayerColour == PlayerColor &&
+                x.PieceType == PieceType.Pawn &&
+                new ConstraintValidator(x, this).IsValid).Single();
+
+            switch(PlayerColor)
+            {
+                case Player.White:
+                    PerformPassant(board, validPiece, 1);
+                    break;
+                case Player.Black:
+                    PerformPassant(board, validPiece, -1);
+                    break;
+            }
+        }
+
+        private void PerformPassant(ChessBoard board, IChessPiece validPiece, int opponentOffset)
+        {
+            var opponent = board[(validPiece.CurrentPosition.Item1, validPiece.CurrentPosition.Item2 + opponentOffset)];
+
+            if (opponent != null && opponent.PlayerColour != PlayerColor)
+            {
+                board.Remove(opponent);
+                board.PerformMove(validPiece, MoveIndex);
+            }
+            else
+            {
+                throw new UserMoveException(this, "En passant capture not valid!");
+            }
+        }
     }
 }
