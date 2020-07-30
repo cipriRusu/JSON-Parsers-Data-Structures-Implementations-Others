3c2109eb31575c287a46f4f00763789efd44d1b9
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-30 14:56:43 +0300

    Replaced ChessBoard parameter with IBoardState type

--- Chess/ChessMoves/Pieces/Knight.cs
+++ Chess/ChessMoves/Pieces/Knight.cs
@@ -12,12 +12,12 @@ namespace ChessMoves
             base(chessBoardIndex, playerColour) =>
             PieceType = PieceType.Knight;
 
-        public bool CanCapture((int, int) target, ChessBoard board) => Captures().Any(x => x.Single() == target);
-        public bool CanReach((int, int) destination, ChessBoard board) => Moves().Any(x => x.Single() == destination);
+        public bool CanCapture((int, int) target, IBoardState board) => Captures().Any(x => x.Single() == target);
+        public bool CanReach((int, int) destination, IBoardState board) => Moves().Any(x => x.Single() == destination);
 
         public override Path Moves() => new Path(CurrentPosition, new PathType[] { PathType.Knight });
         public override Path Captures() => Moves();
-        public override void PerformMove((int, int) targetMove, ChessBoard chessBoard)
+        public override void PerformMove((int, int) targetMove, IBoardState chessBoard)
         {
             var validPath = Moves().Where(x => x.Single() == targetMove).SelectMany(x => x);
             chessBoard.PerformMove(this, targetMove);
