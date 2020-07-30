0e087003243c82c2f4529d81f005f2034a8de33c
Cipri Rusu <cipri91rusu@gmail.com>
2020-07-30 14:48:36 +0300

    Added extra members to IBoardState

--- Chess/ChessMoves/IBoardState.cs
+++ Chess/ChessMoves/IBoardState.cs
@@ -6,9 +6,13 @@ namespace ChessMoves
 {
     public interface IBoardState
     {
+        IChessPiece this[(int, int) index] { get; }
+        bool IsCheckMate { get; set; }
+        bool IsCheck { get; set; }
         IEnumerable<IChessPiece> GetAllPieces();
         void Remove(IChessPiece target);
         bool IsPathClear(IEnumerable<(int, int)> path);
         void PerformMove(IChessPiece chessPiece, (int, int) targetMove);
+        void Promote(IChessPiece piece);
     }
 }
