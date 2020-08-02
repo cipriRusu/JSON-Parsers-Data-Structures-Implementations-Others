0378d6740891fccaf024398c4cd157dda1193409
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-02 12:32:21 +0300

    Moved KingAttack validation logic to ChessBoard

--- Chess/ChessMoves/ChessBoard.cs
+++ Chess/ChessMoves/ChessBoard.cs
@@ -90,6 +90,13 @@ namespace ChessMoves
             }
         }
 
+        public IEnumerable<(int, int)> ValidAttacks(IChessPiece currentKing, PieceType[] attackers, params PathType[] pathTypes) => new Path(currentKing, pathTypes)
+                .Where(x =>
+                            this[x.Last()] != null &&
+                            IsPathClear(x.Skip(1).SkipLast(1)) &&
+                            this[x.Last()].PlayerColour == Piece.Opponent(currentKing.PlayerColour) &&
+                            attackers.Contains(this[x.Last()].PieceType)).SelectMany(x => x);
+
         public void SwitchTurn()
         {
             switch (TurnToMove)
