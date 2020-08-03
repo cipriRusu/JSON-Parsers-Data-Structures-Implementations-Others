97d84ffd60e72d2847f87da5211fd7c2ae722e3a
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-03 09:08:38 +0300

    Removed unencapsulated version of implementations

--- Chess/ChessMoves/Moves/EnPassantUserMove.cs
+++ Chess/ChessMoves/Moves/EnPassantUserMove.cs
@@ -11,38 +11,7 @@ namespace ChessMoves.Moves
 
         public void GetCurrentState(IBoardState board)
         {
-            var validPiece = board.GetAllPieces()
-                .Where(
-                x =>
-                x != null &&
-                x.PlayerColour == PlayerColor &&
-                x.PieceType == PieceType.Pawn &&
-                new ConstraintValidator(x, this).IsValid).Single();
 
-            switch(PlayerColor)
-            {
-                case Player.White:
-                    PerformPassant(board, validPiece, 1);
-                    break;
-                case Player.Black:
-                    PerformPassant(board, validPiece, -1);
-                    break;
-            }
-        }
-
-        private void PerformPassant(IBoardState board, IChessPiece validPiece, int opponentOffset)
-        {
-            var opponent = board[(validPiece.CurrentPosition.Item1, validPiece.CurrentPosition.Item2 + opponentOffset)];
-
-            if (opponent != null && opponent.PlayerColour != PlayerColor)
-            {
-                board.Remove(opponent);
-                board.PerformMove(validPiece, this);
-            }
-            else
-            {
-                throw new UserMoveException(this, "En passant capture not valid!");
-            }
         }
     }
 }
--- Chess/ChessMoves/Moves/QueenCastlingUserMove.cs
+++ Chess/ChessMoves/Moves/QueenCastlingUserMove.cs
@@ -12,57 +12,7 @@ namespace ChessMoves.Moves
 
         public void GetCurrentState(IBoardState board)
         {
-            CastlingExceptions(board);
 
-            var castlingPath = CastlingPath(GetKing(board));
-
-            if (!castlingPath.Skip(1).SkipLast(2).Any(x => new CurrentPlayerStatus(PlayerColor, board).KingPositionCheckStatus(x)))
-            {
-                var king = GetKing(board);
-                var rock = board[castlingPath.Last()];
-
-                king.PerformCastlingMove((king.CurrentPosition.Item1, king.CurrentPosition.Item2 - 2), board);
-                rock.PerformCastlingMove((rock.CurrentPosition.Item1, rock.CurrentPosition.Item2 + 3), board);
-            }
-            else
-            {
-                throw new UserMoveException(this, "Castling move illegal due to CheckPassing");
-            }
-        }
-
-        private IChessPiece GetKing(IBoardState board) =>
-            board.GetAllPieces().Where(x =>
-                                            x != null &&
-                                            x.PieceType == PieceType.King &&
-                                            x.PlayerColour == PlayerColor)
-                                .Single();
-
-        private IEnumerable<(int, int)> CastlingPath(IChessPiece king) =>
-            Enumerable.Range(0, CastlingPathLength)
-                      .Reverse()
-                      .Select(x => (king.CurrentPosition.Item1, x));
-
-        private void CastlingExceptions(IBoardState board)
-        {
-            if (GetKing(board).IsMoved)
-            {
-                throw new UserMoveException(this, "Castling move illegal due to moved King");
-            }
-
-            if (!board.IsPathClear(CastlingPath(GetKing(board)).Skip(1).SkipLast(1)))
-            {
-                throw new UserMoveException(this, "Castling move illegal due to unclear Path");
-            }
-
-            if (board[CastlingPath(GetKing(board)).Last()] == null)
-            {
-                throw new UserMoveException(this, "No castling piece available");
-            }
-
-            if (board[CastlingPath(GetKing(board)).Last()].IsMoved)
-            {
-                throw new UserMoveException(this, "Castling move illegal due to moved Rock");
-            }
         }
     }
 }
