7132a30a5cbf69f9d7df0662faefa6fbc4e66b6e
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-03 08:38:26 +0300

    Removed encapsulated logic from CurrentStatusPlayer/Used King check from encapsulated board.

--- Chess/ChessMoves/CurrentPlayerStatus.cs
+++ Chess/ChessMoves/CurrentPlayerStatus.cs
@@ -10,61 +10,26 @@ namespace ChessMoves
         private readonly Player turnToMove;
         private readonly IBoardState chessBoard;
         private IChessPiece _currentKing;
-        private IChessPiece King => chessBoard.GetAllPieces().Where(x => x != null
-        && x.PieceType == PieceType.King
-        && x.PlayerColour == turnToMove).Single();
 
         public CurrentPlayerStatus(Player turnToMove, IBoardState chessBoard)
         {
             this.turnToMove = turnToMove;
             this.chessBoard = chessBoard;
-            _currentKing = King;
+            _currentKing = chessBoard.GetKing(turnToMove);
         }
 
-        public bool IsChecked => KingCheckStatus();
-
+        public bool IsChecked => chessBoard.IsKingAttackedStatus(turnToMove);
         public bool IsCheckMated => KingCheckMateStatus();
-
         public bool KingPositionCheckStatus(IUserMove move)
         {
             var currentBoardState = chessBoard.DeepClone();
-            currentBoardState.PerformMove(King, move);
+            currentBoardState.PerformMove(_currentKing, move);
             return new CurrentPlayerStatus(turnToMove, currentBoardState).IsChecked;
-        }
-
-        private bool KingCheckStatus()
-        {
-            var diagonalAttacks = chessBoard
-                .ValidAttacks(
-                _currentKing,
-                new PieceType[] { PieceType.Queen, PieceType.Bishop }, 
-                PathType.Diagonals);
-
-            var verticalHorizontalAttacks = chessBoard
-                .ValidAttacks(
-                _currentKing,
-                new PieceType[] { PieceType.Queen, PieceType.Rock },
-                PathType.RowsAndColumns);
-
-            var knightAttacks = chessBoard.
-                ValidAttacks(
-                _currentKing,
-                new PieceType[] { PieceType.Knight },
-                PathType.Knight);
-
-            var pawnAttacks = chessBoard.
-                ValidAttacks(_currentKing,
-                new PieceType[] { PieceType.Pawn },
-                PathType.PawnCapture);
-
-            return diagonalAttacks.Any() || verticalHorizontalAttacks.Any() || knightAttacks.Any() || pawnAttacks.Any();
         }
 
         private bool KingCheckMateStatus()
         {
-            var legalMoves = King.Moves();
-
-            //.Where(x => new UserMove(x.Single(), turnToMove));
+            var legalMoves = _currentKing.Moves();
 
             //foreach (var move in legalMoves)
             //{
