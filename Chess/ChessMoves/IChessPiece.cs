f9640fb2d111981e5ad4bdb35102cec0ed7813a6
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-03 13:15:45 +0300

    Moved/Removed Members from IChessPiece inteface

--- Chess/ChessMoves/IChessPiece.cs
+++ Chess/ChessMoves/IChessPiece.cs
@@ -17,15 +17,9 @@ namespace ChessMoves
         IPath Moves() { return null; }
         IPath Captures() { return null; }
         void Promote(IBoardState chessBoard) { }
-        void MarkPassant(IChessPiece piece, (int, int) destination) { }
-        bool CanReach(IUserMove move, IBoardState chessBoard) => 
-            Moves().Any(x => x.Last() == move.MoveIndex && 
-            chessBoard.IsPathClear(x.Skip(1)));
-        bool CanCapture(IUserMove move, IBoardState chessBoard) => 
-            Captures().Any(x => x.Last() == move.MoveIndex && 
-            chessBoard.IsPathClear(x.Skip(1).SkipLast(1)));
-        virtual void PerformMove((int, int) targetMove, IBoardState chessBoard) { }
-        virtual void PerformCapture((int, int) targetCapture, IBoardState chessBoard) { }
+        void MarkPassant(IChessPiece piece, (int, int) destination);
+        bool CanReach(IUserMove move, IBoardState chessBoard);
+        bool CanCapture(IUserMove move, IBoardState chessBoard);
         void Update(IUserMove move);
     }
 }
