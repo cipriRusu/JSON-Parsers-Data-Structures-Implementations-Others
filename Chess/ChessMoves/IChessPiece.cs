562b4a529c051e0abaf80877bed1dbc3d0c82dec
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-03 08:35:57 +0300

    Used IUserMove as parameter for CanReach/CanCapture

--- Chess/ChessMoves/IChessPiece.cs
+++ Chess/ChessMoves/IChessPiece.cs
@@ -18,13 +18,14 @@ namespace ChessMoves
         IPath Captures() { return null; }
         void Promote(IBoardState chessBoard) { }
         void MarkPassant(IChessPiece piece, (int, int) destination) { }
-        bool CanReach((int, int) destination, IBoardState chessBoard) =>
-            Moves().Any(x => x.Last() == destination && chessBoard.IsPathClear(x.Skip(1)));
-        bool CanCapture((int, int) target, IBoardState chessBoard) =>
-            Captures().Any(x => x.Last() == target && chessBoard.IsPathClear(x.Skip(1).SkipLast(1)));
+        bool CanReach(IUserMove move, IBoardState chessBoard) => 
+            Moves().Any(x => x.Last() == move.MoveIndex && 
+            chessBoard.IsPathClear(x.Skip(1)));
+        bool CanCapture(IUserMove move, IBoardState chessBoard) => 
+            Captures().Any(x => x.Last() == move.MoveIndex && 
+            chessBoard.IsPathClear(x.Skip(1).SkipLast(1)));
         virtual void PerformMove((int, int) targetMove, IBoardState chessBoard) { }
         virtual void PerformCapture((int, int) targetCapture, IBoardState chessBoard) { }
-        //void PerformCastlingMove((int, int) targetMove, IBoardState chessBoard) => chessBoard.PerformMove(this, targetMove);
         void Update(IUserMove move);
     }
 }
