82fc76759065b9ca1464f446ed2b350c6c2a5e39
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-04 15:24:28 +0300

    Simplified member access via constructor / Simplified chained if/else statement

--- Chess/ChessMoves/Moves/MoveType.cs
+++ Chess/ChessMoves/Moves/MoveType.cs
@@ -6,51 +6,28 @@ namespace ChessMoves.Moves
 {
     public class MoveType
     {
-        private readonly string userInput;
-        public IUserMove Move { get; private set; }
-        public Player PlayerTurn { get; private set; }
-        public MoveType(string userInput, Player playerTurn)
-        {
-            this.userInput = userInput;
-            Move = MovementTypeDeterminator(userInput, playerTurn);
-        }
+        public IUserMove Move { get; }
 
-        private IUserMove MovementTypeDeterminator(string input, Player playerTurn)
+        public MoveType(string userInput, Player playerTurn) => Move = MovementTypeDeterminator(userInput, playerTurn);
+
+        private static IUserMove MovementTypeDeterminator(string input, Player playerTurn)
         {
             if (input.Equals("0-0"))
-            {
                 return new KingCastlingUserMove(input, playerTurn);
-            }
-            else if(input.Equals("0-0-0"))
-            {
+            else if (input.Equals("0-0-0"))
                 return new QueenCastlingUserMove(input, playerTurn);
-            }
-            else if(input.EndsWith("+"))
-            {
+            else if (input.EndsWith("+"))
                 return new KingCheckUserMove(input[0..^1], playerTurn);
-            }
-            else if(input.EndsWith("#"))
-            {
+            else if (input.EndsWith("#"))
                 return new KingCheckMateUserMove(input[0..^1], playerTurn);
-            }
             else if (input.Contains('='))
-            {
                 return new PromotionUserMove(input[0..^2], playerTurn);
-            }
             else if (input.EndsWith("e.p."))
-            {
                 return new EnPassantUserMove(input[0..^4], playerTurn);
-            }
             else if (input.Contains("x"))
-            {
                 return new CaptureUserMove(input, playerTurn);
-            }
             else
-            {
                 return new MoveUserMove(input, playerTurn);
-            }
-
-            throw new ArgumentException();
         }
     }
 }
