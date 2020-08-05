5cd363244945d1589d3c30ffb533756cd2d81ec7
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-05 18:31:55 +0300

    Member rename

--- Chess/ChessMoves/IBoardState.cs
+++ Chess/ChessMoves/IBoardState.cs
@@ -13,11 +13,11 @@ namespace ChessMoves
         bool IsCheckMate { get; set; }
         bool IsCheck { get; set; }
         Player TurnToMove { get; }
-        bool IsCastlingValid(IUserMove move);
+        bool CheckCastling(IUserMove move);
         bool CheckPassant(IUserMove enPassantUserMove, out IChessPiece chessPiece);
         IChessPiece GetPiece(IUserMove move);
         void PerformPassant(IUserMove enPassantUserMove, IChessPiece chessPiece);
-        void SetCurrentMove(IUserMove move);
+        void SetMove(IUserMove move);
         IChessPiece GetKing(Player player);
         IEnumerable<IUserMove> GetAllKingMoves(IChessPiece currentKing);
         bool IsPathClear(IEnumerable<(int, int)> path);
