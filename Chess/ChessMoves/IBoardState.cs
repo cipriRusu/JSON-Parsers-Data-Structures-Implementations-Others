a01f812cfeac357ae946ec85d544ec930c492605
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-05 16:00:21 +0300

    Added extra public members for IBoardState

--- Chess/ChessMoves/IBoardState.cs
+++ Chess/ChessMoves/IBoardState.cs
@@ -8,11 +8,14 @@ namespace ChessMoves
     public interface IBoardState
     {
         IChessPiece this[(int, int) index] { get; }
+        IChessPiece this[int first, int second] { get; }
         IChessPiece GetMovablePiece { get; }
         bool IsCheckMate { get; set; }
         bool IsCheck { get; set; }
+        Player TurnToMove { get; }
         bool IsCastlingValid(IUserMove move);
         bool CheckPassant(IUserMove enPassantUserMove, out IChessPiece chessPiece);
+        IChessPiece GetPiece(IUserMove move);
         void PerformPassant(IUserMove enPassantUserMove, IChessPiece chessPiece);
         void SetCurrentMove(IUserMove move);
         IChessPiece GetKing(Player player);
