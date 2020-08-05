bd6e98e9a4e0bf4382ab82fdd8dace1109ed2c10
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-05 18:06:47 +0300

    Extracted EnPassant logic into external class

---
+++ Chess/ChessMoves/PerformPassant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves
+{
+    public class PerformPassant
+    {
+        private ChessBoard chessBoard;
+
+        public PerformPassant(ChessBoard chessBoard) => this.chessBoard = chessBoard;
+
+        internal void Perform(IUserMove move, IChessPiece chessPiece)
+        {
+            switch (move.PlayerColor)
+            {
+                case Player.White:
+                    chessBoard[chessPiece.CurrentPosition.Item1, chessPiece.CurrentPosition.Item2 + 1] = null;
+                    chessBoard.PerformMove(chessPiece, move);
+                    break;
+
+                case Player.Black:
+                    chessBoard[chessPiece.CurrentPosition.Item1, chessPiece.CurrentPosition.Item2 - 1] = null;
+                    chessBoard.PerformMove(chessPiece, move);
+                    break;
+            }
+        }
+    }
+}
