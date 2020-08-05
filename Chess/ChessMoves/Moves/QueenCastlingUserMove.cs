faf9e15a0ae4b519dff5411219062d433eef1462
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-05 13:27:48 +0300

    Method rename

--- Chess/ChessMoves/Moves/QueenCastlingUserMove.cs
+++ Chess/ChessMoves/Moves/QueenCastlingUserMove.cs
@@ -11,7 +11,7 @@ namespace ChessMoves.Moves
 
         public new void GetCurrentState(IBoardState board)
         {
-            if(board.CanPerformCastling(this))
+            if(board.IsCastlingValid(this))
             {
                 board.PerformQueenSideCastling(this);
             }
