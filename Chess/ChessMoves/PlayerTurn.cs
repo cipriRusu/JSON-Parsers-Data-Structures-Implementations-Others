b642cd0c30763e80849c16e0bc41851070715d8a
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-06 11:26:47 +0300

    Extracted player turn switch to external class

---
+++ Chess/ChessMoves/PlayerTurn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves
+{
+    public class PlayerTurn
+    {
+        private Player CurrentTurn;
+        public PlayerTurn(Player currentPlayer) => CurrentTurn = currentPlayer;
+        public Player NextTurn => CurrentTurn == Player.White ? Player.Black : Player.White;
+    }
+}
