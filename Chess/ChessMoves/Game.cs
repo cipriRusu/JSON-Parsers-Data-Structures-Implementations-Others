f157a1a0bd1a08609325c166766caa6845bae74c
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-07 09:57:38 +0300

    Moved board initialization in Game class

--- Chess/ChessMoves/Game.cs
+++ Chess/ChessMoves/Game.cs
@@ -7,11 +7,19 @@ namespace ChessMoves
 {
     public class Game
     {
+        private readonly IChessPiece[,] board = new Piece[8, 8];
+        private readonly IBoardState boardState;
+
+        public Game()
+        {
+            InitializeBlack();
+            InitializeWhite();
+            boardState = new ChessBoard(board);
+        }
+
         public bool IsCheckMate => boardState.IsCheckMate;
         public bool IsCheck => boardState.IsCheck;
         public Player PlayerTurn => boardState.TurnToMove;
-
-        private readonly IBoardState boardState = new ChessBoard();
         public IChessPiece this[int i, int j] => boardState[i, j];
 
         public void Input(string input)
@@ -23,6 +31,48 @@ namespace ChessMoves
                 boardState.GetAndPerform(move);
                 new PlayerTurn(boardState).NextPlayer();
             }
+        }
+
+        private void InitializeBlack()
+        {
+            board[0, 0] = new Rock("a8", Player.Black);
+            board[0, 1] = new Knight("b8", Player.Black);
+            board[0, 2] = new Bishop("c8", Player.Black);
+            board[0, 3] = new Queen("d8", Player.Black);
+            board[0, 4] = new King("e8", Player.Black);
+            board[0, 5] = new Bishop("f8", Player.Black);
+            board[0, 6] = new Knight("g8", Player.Black);
+            board[0, 7] = new Rock("h8", Player.Black);
+
+            board[1, 0] = new Pawn("a7", Player.Black);
+            board[1, 1] = new Pawn("b7", Player.Black);
+            board[1, 2] = new Pawn("c7", Player.Black);
+            board[1, 3] = new Pawn("d7", Player.Black);
+            board[1, 4] = new Pawn("e7", Player.Black);
+            board[1, 5] = new Pawn("f7", Player.Black);
+            board[1, 6] = new Pawn("g7", Player.Black);
+            board[1, 7] = new Pawn("h7", Player.Black);
+        }
+
+        private void InitializeWhite()
+        {
+            board[7, 0] = new Rock("a1", Player.White);
+            board[7, 1] = new Knight("b1", Player.White);
+            board[7, 2] = new Bishop("c1", Player.White);
+            board[7, 3] = new Queen("d1", Player.White);
+            board[7, 4] = new King("e1", Player.White);
+            board[7, 5] = new Bishop("f1", Player.White);
+            board[7, 6] = new Knight("g1", Player.White);
+            board[7, 7] = new Rock("h1", Player.White);
+
+            board[6, 0] = new Pawn("a2", Player.White);
+            board[6, 1] = new Pawn("b2", Player.White);
+            board[6, 2] = new Pawn("c2", Player.White);
+            board[6, 3] = new Pawn("d2", Player.White);
+            board[6, 4] = new Pawn("e2", Player.White);
+            board[6, 5] = new Pawn("f2", Player.White);
+            board[6, 6] = new Pawn("g2", Player.White);
+            board[6, 7] = new Pawn("h2", Player.White);
         }
     }
 }
