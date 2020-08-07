e99bc31586fde77264ed45f25380bbfa4d2a76c0
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-07 10:00:19 +0300

    Moved board initialization logic into external class Implemented board constructor in chessBoard

--- Chess/ChessMoves/ChessBoard.cs
+++ Chess/ChessMoves/ChessBoard.cs
@@ -8,11 +8,7 @@ namespace ChessMoves
     [Serializable]
     public class ChessBoard : IBoardState
     {
-        public ChessBoard()
-        {
-            InitializeWhite();
-            InitializeBlack();
-        }
+        public ChessBoard(IChessPiece[,] board) => this.board = board;
 
         private IChessPiece[,] board = new IChessPiece[ChessboardSize, ChessboardSize];
         private const int ChessboardSize = 8;
@@ -100,9 +96,6 @@ namespace ChessMoves
             Enumerable.Range(0, ChessboardSize).SelectMany(i =>
             Enumerable.Range(0, ChessboardSize).Select(j => board[i, j]));
 
-        private IEnumerable<IUserMove> GetMoveType(IEnumerable<string> input) => 
-            new MovementParser(input).AllMoves;
-
         private void MoveAndPieceExceptions(IEnumerable<IChessPiece> movablePiece)
         {
             if (!movablePiece.Any())
@@ -113,48 +106,6 @@ namespace ChessMoves
             {
                 throw new PieceException("Multiple pieces found that can handle current move");
             }
-        }
-
-        private void InitializeBlack()
-        {
-            board[0, 0] = new Rock("a8", Player.Black);
-            board[0, 1] = new Knight("b8", Player.Black);
-            board[0, 2] = new Bishop("c8", Player.Black);
-            board[0, 3] = new Queen("d8", Player.Black);
-            board[0, 4] = new King("e8", Player.Black);
-            board[0, 5] = new Bishop("f8", Player.Black);
-            board[0, 6] = new Knight("g8", Player.Black);
-            board[0, 7] = new Rock("h8", Player.Black);
-
-            board[1, 0] = new Pawn("a7", Player.Black);
-            board[1, 1] = new Pawn("b7", Player.Black);
-            board[1, 2] = new Pawn("c7", Player.Black);
-            board[1, 3] = new Pawn("d7", Player.Black);
-            board[1, 4] = new Pawn("e7", Player.Black);
-            board[1, 5] = new Pawn("f7", Player.Black);
-            board[1, 6] = new Pawn("g7", Player.Black);
-            board[1, 7] = new Pawn("h7", Player.Black);
-        }
-
-        private void InitializeWhite()
-        {
-            board[7, 0] = new Rock("a1", Player.White);
-            board[7, 1] = new Knight("b1", Player.White);
-            board[7, 2] = new Bishop("c1", Player.White);
-            board[7, 3] = new Queen("d1", Player.White);
-            board[7, 4] = new King("e1", Player.White);
-            board[7, 5] = new Bishop("f1", Player.White);
-            board[7, 6] = new Knight("g1", Player.White);
-            board[7, 7] = new Rock("h1", Player.White);
-
-            board[6, 0] = new Pawn("a2", Player.White);
-            board[6, 1] = new Pawn("b2", Player.White);
-            board[6, 2] = new Pawn("c2", Player.White);
-            board[6, 3] = new Pawn("d2", Player.White);
-            board[6, 4] = new Pawn("e2", Player.White);
-            board[6, 5] = new Pawn("f2", Player.White);
-            board[6, 6] = new Pawn("g2", Player.White);
-            board[6, 7] = new Pawn("h2", Player.White);
         }
     }
 }
