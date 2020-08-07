2460c80a0629fbabf217e45fab1acc61eab32f42
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-07 11:23:13 +0300

    Renamed/Moved input extraction logic

---
+++ Chess/DisplayChessBoard/FileInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ChessMoves
+{
+    public class FileInput
+    {
+        public string GetFileContent() => 
+            new StreamReader(@"C:\Users\sysuser\Desktop\Reps\JMRepo\Chess\ChessMoves\Moves.txt").ReadToEnd().Replace("\r\n", "");
+    }
+}
