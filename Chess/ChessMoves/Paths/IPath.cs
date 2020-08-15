8db528eb26e0ef9e6633fbfe8afaf82be422a09e
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-15 11:38:43 +0300

    Created abstraction for Path type

---
+++ Chess/ChessMoves/Paths/IPath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves.Paths
+{
+    public interface IPath
+    {
+        (int, int) Start { get; }
+        (int, int) End { get; }
+        IEnumerable<(int, int)> Path { get; }
+        bool ReachesTarget(IUserMove move);
+    }
+}
