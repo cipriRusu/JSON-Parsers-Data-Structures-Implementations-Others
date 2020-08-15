fa1974ae421f60d82b27f188e8c4964dfc6c7a86
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-15 11:39:09 +0300

    Created Path base class

---
+++ Chess/ChessMoves/Paths/Path.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ChessMoves.Paths
+{
+    public class Path : IPath
+    {
+        private IEnumerable<(int, int)> FullPath;
+        public Path(IEnumerable<(int, int)> path, (int, int) pathStart)
+        {
+            FullPath = path;
+            if (path.Count() >= 2)
+            {
+                Start = pathStart;
+                End = FullPath.Last();
+            }
+            else
+            {
+                Start = pathStart;
+                End = FullPath.Single();
+            }
+        }
+
+        public (int, int) Start { get; }
+        public (int, int) End { get; }
+        IEnumerable<(int, int)> IPath.Path => FullPath;
+        public bool ReachesTarget(IUserMove move) => move.MoveIndex == End;
+    }
+}
