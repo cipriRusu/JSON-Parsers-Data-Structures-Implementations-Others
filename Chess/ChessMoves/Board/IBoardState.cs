85b8173ab349cbe7b38b563a1bf0eff0bbd5c7d0
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-18 23:43:11 -0700

    Hidden PieceToMove property (moved all interaction into class) Hidden IChessPiece from public member (interaction via IUserMove)

--- Chess/ChessMoves/Board/IBoardState.cs
+++ Chess/ChessMoves/Board/IBoardState.cs
@@ -10,18 +10,16 @@ namespace ChessMoves
     {
         IChessPiece this[(int, int) index] { get; }
         IChessPiece this[int first, int second] { get; }
-        IChessPiece PieceToMove { get; }
         bool CheckCastling(IUserMove move);
         bool CheckPassant(IUserMove enPassantUserMove, out IChessPiece chessPiece);
         IChessPiece GetPiece(IUserMove move);
         void GetAndPerform(IUserMove move);
         void PerformPassant(IUserMove enPassantUserMove, IChessPiece chessPiece);
-        void SetMove(IUserMove move);
+        void PerformMove(IUserMove move);
         IChessPiece GetKing(Player player);
         IEnumerable<IUserMove> AllKingMoves(IChessPiece currentKing);
         bool IsMovePathClear(IPath input);
         void PerformCastling(IUserMove move);
-        void PerformMove(IChessPiece chessPiece, IUserMove move);
         void Promote(IChessPiece piece);
         void Remove(IChessPiece target);
         bool IsCapturePathClear(IPath path);
