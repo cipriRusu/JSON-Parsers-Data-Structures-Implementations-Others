b45b244ee6e6533f180e2ae28ec13df6b24c19ce
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-20 13:40:21 +0300

    Implemented IMovable interface for piece moves

---
+++ Chess/ChessMoves/IMovable.cs
@@ -0,0 +1,13 @@
+﻿using ChessMoves.Paths;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves
+{
+    interface IMovable
+    {
+        IEnumerable<IPath> Moves();
+        IEnumerable<IPath> Captures();
+    }
+}
