82ec3326ae4a356ad2953811c232f128cadb0033
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-21 01:16:42 +0300

    Created class for  board/move direct interaction

---
+++ Chess/ChessMoves/Moves/Move.cs
@@ -0,0 +1,29 @@
+﻿using ChessMoves;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Moves
+{
+    public class Move
+    {
+        private IPiece[,] board;
+        private IPiece currentMovablePiece;
+
+        public Move(IPiece[,] board, IPiece currentMovablePiece)
+        {
+            this.board = board;
+            this.currentMovablePiece = currentMovablePiece;
+        }
+
+        internal void ApplyMove(IUserMove move)
+        {
+            var (firstIndex, secondIndex) = currentMovablePiece.Index;
+
+            board[move.Index.Item1, move.Index.Item2] =
+            board[currentMovablePiece.Index.Item1, currentMovablePiece.Index.Item2];
+
+            board[firstIndex, secondIndex] = null;
+        }
+    }
+}
