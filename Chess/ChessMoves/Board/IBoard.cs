8475b4a21f4fb695242bda075f454482d284fb97
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-21 00:50:29 +0300

    Simplified IBoard members

--- Chess/ChessMoves/Board/IBoard.cs
+++ Chess/ChessMoves/Board/IBoard.cs
@@ -1,5 +1,6 @@
 ﻿using ChessMoves.Moves;
 using ChessMoves.Paths;
+using Newtonsoft.Json.Bson;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,20 +9,7 @@ namespace ChessMoves
 {
     public interface IBoard
     {
-        IPiece this[(int, int) index] { get; }
-        IPiece this[int first, int second] { get; }
-        bool CheckCastling(IUserMove move);
-        bool CheckPassant(IUserMove enPassantUserMove, out IPiece chessPiece);
-        IPiece GetPiece(IUserMove move);
+        public IPiece this[int first, int second] { get; }
         void Perform(IUserMove move);
-        void PerformPassant(IUserMove enPassantUserMove, IPiece chessPiece);
-        void PerformMove(IUserMove move);
-        IPiece GetKing(Player player);
-        IEnumerable<IUserMove> AllKingMoves(IPiece currentKing);
-        bool IsMovePathClear(IPath input);
-        void PerformCastling(IUserMove move);
-        void Promote(IPiece piece);
-        void Remove(IPiece target);
-        bool IsCapturePathClear(IPath path);
     }
 }
