a3ef5ed14e3e350f36bb7a5dc6b1fbd5719fab85
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-21 00:44:13 +0300

    Implemented Class for move/piece interaction (move and path validation)

---
+++ Chess/ChessMoves/LegalPiece.cs
@@ -0,0 +1,28 @@
+﻿using ChessMoves;
+using ChessMoves.Paths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame
+{
+    public class LegalPiece
+    {
+        private readonly IEnumerable<IPiece> AllPieces;
+        public LegalPiece(IEnumerable<IPiece> allPieces) => AllPieces = allPieces;
+        public IPiece GetMovablePiece(IUserMove move, out IPath path)
+        {
+            var piece = AllPieces
+                .Where(x => x != null)
+                .Where(x => x.PlayerColour == move.PlayerColor)
+                .Where(x => x.GetType() == move.PieceType)
+                .Where(x => x.CanPerform(move));
+
+            var legalPath = piece.Single().GetPath(move);
+
+            path = legalPath;
+            return piece.Single();
+        }
+    }
+}
