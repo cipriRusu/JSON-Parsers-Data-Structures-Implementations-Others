a148214b6cd9b3e773c087073ce8fc6ab6df7027
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-22 01:17:42 +0300

    Implemented HasPath member for selecting valid path

--- Chess/ChessMoves/Moves/MoveUserMove.cs
+++ Chess/ChessMoves/Moves/MoveUserMove.cs
@@ -9,6 +9,6 @@ namespace ChessMoves.Moves
     public class MoveUserMove : UserMove, IUserMove
     {
         public MoveUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
-        public override bool ContainsValidPath(IEnumerable<IPath> allPaths) => allPaths.Any(x => x.End == Index);
+        public override bool HasPath(IEnumerable<IPath> allPaths) => allPaths.Any(x => x.End == Index);
     }
 }
