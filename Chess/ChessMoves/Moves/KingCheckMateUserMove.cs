cad71208eeb44bb2a9427d080bc4ecd254c6337e
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-22 01:18:53 +0300

    Removed former implementation (moved validation to King piece)

--- Chess/ChessMoves/Moves/KingCheckMateUserMove.cs
+++ Chess/ChessMoves/Moves/KingCheckMateUserMove.cs
@@ -7,20 +7,5 @@ namespace ChessMoves.Moves
     public class KingCheckMateUserMove : UserMove, IUserMove
     {
         public KingCheckMateUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
-
-        public new void GetCurrentState(IBoard board)
-        {
-            var internalMove = new MoveType(NotationIndex, PlayerColor).Move;
-            internalMove.GetCurrentState(board);
-
-            if (new CurrentPlayerStatus(Piece.Opponent(PlayerColor), board).IsCheckMated)
-            {
-                //TODO
-            }
-            else
-            {
-                new UserMoveException(this, "Check move not valid");
-            }
-        }
     }
 }
--- Chess/ChessMoves/Moves/KingCheckUserMove.cs
+++ Chess/ChessMoves/Moves/KingCheckUserMove.cs
@@ -7,20 +7,5 @@ namespace ChessMoves.Moves
     public class KingCheckUserMove : UserMove, IUserMove
     {
         public KingCheckUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
-
-        public new void GetCurrentState(IBoard board)
-        {
-            var internalMove = new MoveType(NotationIndex, PlayerColor).Move;
-            internalMove.GetCurrentState(board);
-
-            if (new CurrentPlayerStatus(Piece.Opponent(PlayerColor), board).IsChecked)
-            {
-                //TODO
-            }
-            else
-            {
-                throw new UserMoveException(this, "Check move not valid");
-            }
-        }
     }
 }
