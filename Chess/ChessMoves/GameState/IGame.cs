49e82fea24193772ad411c880da5a01aad3758ce
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-21 18:43:22 +0300

    Added indexer member for Game

--- Chess/ChessMoves/GameState/IGame.cs
+++ Chess/ChessMoves/GameState/IGame.cs
@@ -6,6 +6,7 @@ namespace ChessMoves
 {
     public interface IGame
     {
+        public IPiece this[int first, int second] { get; }
         Player TurnToMove { get; set; }
         bool IsCheckMate { get; }
         bool IsCheck { get; }
