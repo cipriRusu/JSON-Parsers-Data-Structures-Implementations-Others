6e6f94cd786c3455b9f73ca14069d56e1b2ad6f8
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-22 01:14:24 +0300

    Implemented HasPath method in CaptureUserMove class (for path selection)

--- Chess/ChessMoves/Moves/CaptureUserMove.cs
+++ Chess/ChessMoves/Moves/CaptureUserMove.cs
@@ -9,6 +9,6 @@ namespace ChessMoves
     public class CaptureUserMove : UserMove, IUserMove
     {
         public CaptureUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
-        public override bool ContainsValidPath(IEnumerable<IPath> allPaths) => allPaths.Any(x => x.End == Index);
+        public override bool HasPath(IEnumerable<IPath> allPaths) => allPaths.Any(x => x.End == Index);
     }
 }
