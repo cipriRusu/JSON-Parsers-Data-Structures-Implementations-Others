ada1dac60ab8171a8b347dde14b4432bb312cc43
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-21 19:14:07 +0300

    Removed unused members from piece classes

--- Chess/ChessMoves/Pieces/Knight.cs
+++ Chess/ChessMoves/Pieces/Knight.cs
@@ -13,15 +13,7 @@ namespace ChessMoves
             base(chessBoardIndex, playerColour) =>
             PieceType = typeof(Knight);
 
-        public bool CanCapture((int, int) target, IBoard board) => Captures().Any(x => x.End == target);
-        public bool CanReach((int, int) destination, IBoard board) => Moves().Any(x => x.End == destination);
-
         public override IEnumerable<IPath> Moves() => new PathGenerator(this, PathType.Knight).GetEnumerator();
         public override IEnumerable<IPath> Captures() => Moves();
-        public override void PerformMove(IUserMove move, IBoard chessBoard)
-        {
-            var validPath = Moves().Where(x => x.End == move.Index);
-            chessBoard.PerformMove(move);
-        }
     }
 }
--- Chess/ChessMoves/Pieces/Pawn.cs
+++ Chess/ChessMoves/Pieces/Pawn.cs
@@ -1,28 +1,22 @@
-﻿using ChessMoves.Paths;
+﻿using ChessMoves.Moves;
+using ChessMoves.Paths;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
+using Xunit.Sdk;
 
 namespace ChessMoves
 {
     [Serializable]
     public class Pawn : Piece, IPiece
     {
-        public Pawn(string chessBoardIndex, Player playerColour) : base(chessBoardIndex, playerColour) => 
+        public Pawn(string chessBoardIndex, Player playerColour) : base(chessBoardIndex, playerColour) =>
             PieceType = typeof(Pawn);
 
         public override IEnumerable<IPath> Moves() => new PathGenerator(this, PathType.Pawn).GetEnumerator();
         public override IEnumerable<IPath> Captures() => new PathGenerator(this, PathType.PawnCapture).GetEnumerator();
-        public override void PerformCapture(IUserMove move, IBoard chessBoard)
-        {
-            var targetPiece = chessBoard[move.Index];
-
-            if (Opponent(PlayerColour) == targetPiece.PlayerColour)
-            {
-                chessBoard.PerformMove(move);
-            }
-        }
     }
 }
