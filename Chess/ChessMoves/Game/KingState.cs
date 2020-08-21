225695b43f83f76688100fc21a22709c50588b9f
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-21 22:22:16 +0300

    Implemented KingState class for determining King state

---
+++ Chess/ChessMoves/Game/KingState.cs
@@ -0,0 +1,29 @@
+﻿using ChessMoves;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ChessGame.Game
+{
+    public class KingState
+    {
+        private IBoard board;
+        public KingState(IBoard board) => this.board = board;
+
+        public bool Checked(IUserMove move)
+        {
+            var king = (IKing)board.Where(x => x is IKing && x.PlayerColour != move.PlayerColor).Single();
+
+            return king.IsChecked(board);
+        }
+
+        public bool CheckMated(IUserMove move)
+        {
+            var king = (IKing)board.Where(x => x is IKing && x.PlayerColour != move.PlayerColor).Single();
+            
+            return king.IsCheckMate(board);
+        }
+    }
+}
