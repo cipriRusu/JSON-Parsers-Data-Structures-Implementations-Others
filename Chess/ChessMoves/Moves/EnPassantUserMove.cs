36febe4dd59d593573d7a823e5dc5da9047270ff
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-22 01:14:43 +0300

    Cleared former implementation(TODO)

--- Chess/ChessMoves/Moves/EnPassantUserMove.cs
+++ Chess/ChessMoves/Moves/EnPassantUserMove.cs
@@ -8,17 +8,5 @@ namespace ChessMoves.Moves
     public class EnPassantUserMove : UserMove, IUserMove
     {
         public EnPassantUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
-
-        public new void GetCurrentState(IBoard board)
-        {
-            if(board.CheckPassant(this, out IPiece chessPiece))
-            {
-                board.PerformPassant(this, chessPiece);
-            }
-            else
-            {
-                throw new UserMoveException("Illegal EnPassant move!!");
-            }
-        }
     }
 }
