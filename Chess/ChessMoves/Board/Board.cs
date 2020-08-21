6b63921cf64c6b33d11bd63fc1e96a153630290e
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-21 18:41:00 +0300

    Removed serializable decorator Implemented path checking method Implemented handling for Check/CheckMate moves

--- Chess/ChessMoves/Board/Board.cs
+++ Chess/ChessMoves/Board/Board.cs
@@ -9,33 +9,48 @@ using System.Linq;
 
 namespace ChessMoves
 {
-    [Serializable]
     public class Board : IBoard
     {
         private const int ChessboardSize = 8;
         private IPiece[,] board = new IPiece[ChessboardSize, ChessboardSize];
         public Board(IPiece[,] board) => this.board = board;
         public IPiece this[int first, int second] => board[first, second];
-
+        public bool IsPathClear(IPath path) => path.Path.Skip(1).SkipLast(1).All(x => board[x.Item1, x.Item2] == null);
         public void Perform(IUserMove move)
         {
             if (move is MoveUserMove)
                 PerformMove(move);
             else if (move is CaptureUserMove)
                 PerformCapture(move);
+            else if (move is KingCheckUserMove)
+                PerformCheck(move);
+            else if (move is KingCheckMateUserMove)
+                PerformCheckMate(move);
         }
 
         private void PerformMove(IUserMove move)
         {
             var legalPiece = new LegalPiece(GetAllPieces()).GetMovablePiece(move, out IPath path);
 
-            if(new MoveValidator(board, move).ValidatePath(path))
+            if (new MoveValidator(board, move).ValidatePath(path))
             {
                 new Move(board, legalPiece).ApplyMove(move);
             }
         }
 
         private void PerformCapture(IUserMove move) => PerformMove(move);
+
+        private void PerformCheck(IUserMove move)
+        {
+            var internalMove = new MoveType(move.NotationIndex, move.PlayerColor).Move;
+            Perform(internalMove);
+        }
+
+        private void PerformCheckMate(IUserMove move)
+        {
+            var internalMove = new MoveType(move.NotationIndex, move.PlayerColor).Move;
+            Perform(internalMove);
+        }
 
         private IEnumerable<IPiece> GetAllPieces() =>
             Enumerable.Range(0, ChessboardSize).SelectMany(i =>
