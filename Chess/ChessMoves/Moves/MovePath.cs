093e3e7aef0324a26203ccbb33b37c1e88631d73
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-22 01:00:49 +0300

    Implemented PathChecking selection class

---
+++ Chess/ChessMoves/Moves/MovePath.cs
@@ -0,0 +1,26 @@
+﻿using ChessMoves;
+using ChessMoves.Moves;
+using ChessMoves.Paths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame.Moves
+{
+    class MovePath
+    {
+        public MovePath(Piece piece) => Piece = piece;
+
+        public Piece Piece { get; }
+
+        internal IPath GetPath(IUserMove move)
+        {
+            if (move is MoveUserMove)
+                return Piece.Moves().Where(x => x.End == move.Index).Single();
+            else if (move is CaptureUserMove)
+                return Piece.Captures().Where(x => x.End == move.Index).Single();
+            return null;
+        }
+    }
+}
