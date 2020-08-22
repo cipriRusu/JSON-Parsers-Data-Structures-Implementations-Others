02526239e82b95fc83f630eb1e938b14d7e60b30
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-22 21:03:27 +0300

    Implemented castling as move

--- Chess/ChessMoves/Moves/Move.cs
+++ Chess/ChessMoves/Moves/Move.cs
@@ -10,6 +10,8 @@ namespace ChessGame.Moves
         private IPiece[,] board;
         private IPiece piece;
 
+        public Move(IPiece[,] board) => this.board = board;
+
         public Move(IPiece[,] board, IPiece currentMovablePiece)
         {
             this.board = board;
@@ -23,14 +25,57 @@ namespace ChessGame.Moves
             board[move.Index.Item1, move.Index.Item2] =
             board[piece.Index.Item1, piece.Index.Item2];
             board[piece.Index.Item1, piece.Index.Item2].UpdateAfterMove(move);
-            
-            if(board[piece.Index.Item1, piece.Index.Item2] is ICastable)
+
+            if (board[piece.Index.Item1, piece.Index.Item2] is ICastable)
             {
                 var moved = (ICastable)board[piece.Index.Item1, piece.Index.Item2];
                 moved.IsMoved = true;
             }
 
             board[firstIndex, secondIndex] = null;
+        }
+
+        internal void ApplyKingCastling(IUserMove move)
+        {
+            switch (move.PlayerColor)
+            {
+                case Player.White:
+                    (board[7, 5], board[7, 7]) = (board[7, 7], board[7, 5]);
+                    (board[7, 6], board[7, 4]) = (board[7, 4], board[7, 6]);
+                    board[7, 5].UpdateAfterMove(move);
+                    board[7, 6].UpdateAfterMove(move);
+                    break;
+
+                case Player.Black:
+                    (board[0, 5], board[0, 7]) = (board[0, 7], board[0, 5]);
+                    (board[0, 6], board[0, 4]) = (board[0, 4], board[0, 6]);
+                    board[0, 5].UpdateAfterMove(move);
+                    board[0, 6].UpdateAfterMove(move);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        internal void ApplyQueenCastling(IUserMove move)
+        {
+            switch (move.PlayerColor)
+            {
+                case Player.White:
+                    (board[7, 3], board[7, 0]) = (board[7, 0], board[7, 3]);
+                    (board[7, 2], board[7, 4]) = (board[7, 4], board[7, 2]);
+                    board[7, 3].UpdateAfterMove(move);
+                    board[7, 2].UpdateAfterMove(move);
+                    break;
+                case Player.Black:
+                    (board[0, 3], board[0, 0]) = (board[0, 0], board[0, 3]);
+                    (board[0, 2], board[0, 4]) = (board[0, 4], board[0, 2]);
+                    board[0, 3].UpdateAfterMove(move);
+                    board[0, 2].UpdateAfterMove(move);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
