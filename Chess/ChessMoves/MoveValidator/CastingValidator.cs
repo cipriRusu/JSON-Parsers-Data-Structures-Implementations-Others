8830c25a6d94555033a7a67a0c6a75a7f5969671
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-22 04:29:29 +0300

    Implemented CastlingValidation class (TODO)

---
+++ Chess/ChessMoves/MoveValidator/CastingValidator.cs
@@ -0,0 +1,26 @@
+﻿using ChessMoves;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame
+{
+    public class CastingValidator
+    {
+        private IBoard board;
+        public CastingValidator(IBoard board) => this.board = board;
+
+        public void IsValid(Player player, bool IsKingSide)
+        {
+            var castlings = board
+                .Where(x => x is ICastable)
+                .Where(x => x.PlayerColour == player);
+
+            var king = castlings.Where(x => x is IKing);
+            var rock = castlings.Where(x => x is IRock).Select(x => (IRock)x);
+
+            rock = IsKingSide ? rock.Where(x => x.IsKingSide) : rock.Where(x => !x.IsKingSide);
+        }
+    }
+}
