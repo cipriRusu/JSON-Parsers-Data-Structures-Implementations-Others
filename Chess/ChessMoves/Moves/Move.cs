deb7830417e0e3affd3b0d7b4e67425f837df73e
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-22 05:40:03 +0300

    Implemented IsMoved property for Castling verification in Board

--- Chess/ChessMoves/Moves/Move.cs
+++ Chess/ChessMoves/Moves/Move.cs
@@ -23,6 +23,13 @@ namespace ChessGame.Moves
             board[move.Index.Item1, move.Index.Item2] =
             board[piece.Index.Item1, piece.Index.Item2];
             board[piece.Index.Item1, piece.Index.Item2].UpdateAfterMove(move);
+            
+            if(board[piece.Index.Item1, piece.Index.Item2] is ICastable)
+            {
+                var moved = (ICastable)board[piece.Index.Item1, piece.Index.Item2];
+                moved.IsMoved = true;
+            }
+
             board[firstIndex, secondIndex] = null;
         }
     }
