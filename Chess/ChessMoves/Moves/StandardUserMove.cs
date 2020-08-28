302a36c991d1eb4b3a52749151546e13e7e5d44a
Cipri Rusu <cipri91rusu@gmail.com>
2020-08-28 21:39:31 +0300

    Renamed UserMove class (For clarity)

---
+++ Chess/ChessMoves/Moves/StandardUserMove.cs
@@ -0,0 +1,28 @@
+﻿using ChessGame.Interfaces;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ChessMoves.Moves
+{
+    public class StandardUserMove : UserMove, IUserMove
+    {
+        public StandardUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
+
+        public override bool CanReach(IBoardOperation boardOperation, IPathTypes pathTypes)
+        {
+            var path = pathTypes.Moves.Where(x => x.End == Index);
+
+            return path.Count() > 0 && boardOperation.IsClear(path.Single(), 1, 0);
+        }
+
+        public override void Move(IBoardOperation boardOperation)
+        {
+            if (!boardOperation.CurrentPieces.Any()) throw new UserMoveException(this, "No pieces!!");
+
+            if (boardOperation.CurrentPieces.Count() > 1) throw new UserMoveException(this, "Multiple pieces!!");
+
+            boardOperation.Apply(boardOperation.CurrentPieces.Single(), this);
+        }
+    }
+}
