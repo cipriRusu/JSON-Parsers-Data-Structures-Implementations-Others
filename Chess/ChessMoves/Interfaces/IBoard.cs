e55efd9608349e5f2c5b6094dcb88f8ea7913799
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-01 14:27:49 +0300

    Removed unencapsulated member

--- Chess/ChessMoves/Interfaces/IBoard.cs
+++ Chess/ChessMoves/Interfaces/IBoard.cs
@@ -1,15 +1,9 @@
-﻿using ChessMoves.Moves;
-using ChessMoves.Paths;
-using Newtonsoft.Json.Bson;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ChessMoves.Paths;
 
 namespace ChessMoves
 {
     public interface IBoard
     {
-        public IPiece this[(int, int) input] { get; }
         void Perform(IUserMove move);
         bool IsClear(IPath path, int frontSkip = 0, int endSkip = 0);
     }
