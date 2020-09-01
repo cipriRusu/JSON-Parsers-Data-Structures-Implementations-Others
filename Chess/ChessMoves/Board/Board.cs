cb78869fff5f1ca07d95b89b8f06726bddce37b8
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-01 17:55:02 +0300

    Simplified IsClear implementation (Reimplementation)

--- Chess/ChessMoves/Board/Board.cs
+++ Chess/ChessMoves/Board/Board.cs
@@ -5,16 +5,12 @@ using System.Linq;
 
 namespace ChessMoves
 {
-    public class Board : IBoard, IBoardOperation
+    public class Board : IBoard, IOperation
     { 
         private const int ChessboardSize = 8;
         private IPiece[,] board = new IPiece[ChessboardSize, ChessboardSize];
         public Board(IPiece[,] board) => this.board = board;
-
-        public bool IsClear(IPath path, int frontSkip = 0, int endSkip = 0) => 
-            path.Path.Skip(frontSkip)
-                     .SkipLast(endSkip)
-                     .All(x => board[x.Item1, x.Item2] == null);
+        public bool IsClear(IPath path) => path.Path.All(x => board[x.Item1, x.Item2] == null);
 
         public void Perform(IUserMove move)
         {
