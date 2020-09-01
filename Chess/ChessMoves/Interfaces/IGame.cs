25976b925a38dd4a25f6c9ff8461462a9a3cc2ac
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-01 14:28:58 +0300

    Removed unusable member (forced encapsulation)

--- Chess/ChessMoves/Interfaces/IGame.cs
+++ Chess/ChessMoves/Interfaces/IGame.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ChessMoves
+﻿namespace ChessMoves
 {
     public interface IGame
     {
-        public IPiece this[(int, int) input] { get; }
         Player TurnToMove { get; set; }
         bool IsCheckMate { get; }
         bool IsCheck { get; }
