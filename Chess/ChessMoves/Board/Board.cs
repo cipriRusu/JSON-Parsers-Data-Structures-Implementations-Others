42c52631d4d82443a25f75ca0dae3b3471691588
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-01 14:26:48 +0300

    Removed unused members (Reimplementation)

--- Chess/ChessMoves/Board/Board.cs
+++ Chess/ChessMoves/Board/Board.cs
@@ -9,16 +9,8 @@ namespace ChessMoves
     { 
         private const int ChessboardSize = 8;
         private IPiece[,] board = new IPiece[ChessboardSize, ChessboardSize];
-        public delegate void Move(IPath path);
         public Board(IPiece[,] board) => this.board = board;
-        public IPiece this[(int, int) input]
-        {
-            get => board[input.Item1, input.Item2];
-            set => board[input.Item1, input.Item2] = value; 
-        }
 
-        public IUserMove CurrentMove { get; private set; }
-        public IEnumerable<IPiece> CurrentPieces { get; private set; }
         public bool IsClear(IPath path, int frontSkip = 0, int endSkip = 0) => 
             path.Path.Skip(frontSkip)
                      .SkipLast(endSkip)
@@ -26,21 +18,8 @@ namespace ChessMoves
 
         public void Perform(IUserMove move)
         {
-            CurrentMove = move;
 
-            CurrentPieces = AllPieces().Where(x => x != null).Where(x => x.CanPerform(this));
-
-            CurrentMove.Move(this);
         }
-
-        public void Apply(IPiece piece, IUserMove move)
-        {
-            this[move.Index] = this[piece.Index];
-            this[piece.Index] = null;
-            this[move.Index].Update(move);
-        }
-
-        public bool IsOpponent(IPath path, Player player) => this[path.End].PlayerColour != player;
 
         private IEnumerable<IPiece> AllPieces()
         {
