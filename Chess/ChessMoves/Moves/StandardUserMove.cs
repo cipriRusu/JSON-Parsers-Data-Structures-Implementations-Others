a4bc4eec9c741242de28bc7158941b0765a58932
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-02 15:06:36 +0300

    Implemented StandardUserMove logic for selection of movable piece (incomplete) TODO : Path implementation/Validation in board

--- Chess/ChessMoves/Moves/StandardUserMove.cs
+++ Chess/ChessMoves/Moves/StandardUserMove.cs
@@ -1,7 +1,24 @@
-﻿namespace ChessMoves.Moves
+﻿using ChessGame.Interfaces;
+using System.Linq;
+
+namespace ChessMoves.Moves
 {
     public class StandardUserMove : UserMove, IUserMove
     {
         public StandardUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
+
+        public override bool CanHandle(IPieceValidator validator, IBoardCheck boardCheck)
+        {
+            if (validator.IsPiece(this))
+            {
+                var path = validator.Moves.Where(x => x.End == Index);
+
+                return path.Any() 
+                    && (boardCheck.IsClear(path.Single()) ? true : 
+                    throw new UserMoveException(this, "Illegal move due to unclear path!"));
+            }
+
+            return false;
+        }
     }
 }
