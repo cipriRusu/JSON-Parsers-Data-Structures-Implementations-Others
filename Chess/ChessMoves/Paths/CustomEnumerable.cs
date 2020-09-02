508500e69725a07ee563b5e4b85e1e980f1b2860
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-02 18:43:48 +0300

    Implemented extension methods for path generation

---
+++ Chess/ChessMoves/Paths/CustomEnumerable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame.Paths
+{
+    public static class CustomEnumerable
+    {
+        public static IEnumerable<int> CountUp(int from, int to)
+        {
+            while (from <= to)
+            {
+                yield return from;
+                from++;
+            }
+        }
+        public static IEnumerable<int> CountDown(int from, int to)
+        {
+            while(from >= to)
+            {
+                yield return from;
+                from--;
+            }
+        }
+    }
+}
