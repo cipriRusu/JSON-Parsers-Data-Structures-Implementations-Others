00501f85c51e249749026354ee91768bc781a819
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-02 15:02:59 +0300

    Implemented piece selection method for movable piece (incomplete) Removed IsClear implementation (temporary) Renamed IOperation/IBoardCheck (incomplete) TODO : Implemented path / path validation (polymorphic)

--- Chess/ChessMoves/Board/Board.cs
+++ Chess/ChessMoves/Board/Board.cs
@@ -5,16 +5,16 @@ using System.Linq;
 
 namespace ChessMoves
 {
-    public class Board : IBoard, IOperation
+    public class Board : IBoard, IBoardCheck
     { 
         private const int ChessboardSize = 8;
         private IPiece[,] board = new IPiece[ChessboardSize, ChessboardSize];
         public Board(IPiece[,] board) => this.board = board;
-        public bool IsClear(IPath path) => path.Path.All(x => board[x.Item1, x.Item2] == null);
+        public bool IsClear(IPath path) => true;
 
         public void Perform(IUserMove move)
         {
-
+            var performers = AllPieces().Where(x => x != null && x.CanPerform(move, this));
         }
 
         private IEnumerable<IPiece> AllPieces()
