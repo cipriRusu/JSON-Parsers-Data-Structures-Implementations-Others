e885c2a4dccf95267eff62f21a996a57bd470267
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-02 15:05:14 +0300

    Implemented PieceValidator abstraction on Piece (for piece validation on move)

---
+++ Chess/ChessMoves/Interfaces/IPieceValidator.cs
@@ -0,0 +1,12 @@
+﻿using ChessMoves;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Interfaces
+{
+    public interface IPieceValidator : IPathTypes
+    {
+        bool IsPiece(IUserMove userMove);
+    }
+}
