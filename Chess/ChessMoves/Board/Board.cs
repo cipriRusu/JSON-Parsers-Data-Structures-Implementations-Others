a72ed7775f6a69c6b9b661db56465b4888815ef3
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-07 08:24:23 +0300

    Implemented piece checking/returning and movement into board

--- Chess/ChessMoves/Board/Board.cs
+++ Chess/ChessMoves/Board/Board.cs
@@ -1,20 +1,32 @@
 ﻿using ChessGame.Interfaces;
 using ChessMoves.Paths;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ChessMoves
 {
-    public class Board : IBoard, IBoardCheck
+    public class Board : IBoard, IMoveCheck, IMovePerform
     { 
         private const int ChessboardSize = 8;
         private IPiece[,] board = new IPiece[ChessboardSize, ChessboardSize];
         public Board(IPiece[,] board) => this.board = board;
         public bool IsClear(IPath path) => path.Path.All(x => board[x.Item1, x.Item2] == null);
+        public IEnumerable<IPiece> Performers { get; private set; }
 
-        public void Perform(IUserMove move)
+        public void GetCurrent(IUserMove move)
         {
-            var performers = AllPieces().Where(x => x != null && x.CanPerform(move, this));
+            Performers = AllPieces().Where(x => x != null && x.CanPerform(move, this));
+
+            move.CallBack(MoveTo);
+
+            move.Perform(this);
+        }
+
+        private void MoveTo(IPiece piece, (int, int) Target)
+        {
+            board[Target.Item1, Target.Item2] = piece;
+            board[piece.Index.Item1, piece.Index.Item2] = null;
         }
 
         private IEnumerable<IPiece> AllPieces()
