3a3b5f4f2522d8aca0f8d2824f8700ba6d223afd
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-07 08:16:06 +0300

    Implemnented IMovePerform interface/abstraction for accessing Performer pieces

---
+++ Chess/ChessMoves/Interfaces/IMovePerform.cs
@@ -0,0 +1,12 @@
+﻿using ChessMoves;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Interfaces
+{
+    public interface IMovePerform
+    {
+        IEnumerable<IPiece> Performers { get; }
+    }
+}
