c5e5569f8087cc983ca3e589d617ed25dc7cabd0
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-07 08:21:54 +0300

    Implemented basic StandardUserMove

--- Chess/ChessMoves/Moves/StandardUserMove.cs
+++ Chess/ChessMoves/Moves/StandardUserMove.cs
@@ -1,4 +1,5 @@
 ﻿using ChessGame.Interfaces;
+using System;
 using System.Linq;
 
 namespace ChessMoves.Moves
@@ -7,18 +8,25 @@ namespace ChessMoves.Moves
     {
         public StandardUserMove(string input, Player playerTurn) : base(input, playerTurn) { }
 
-        public override bool CanHandle(IValidate validator, IBoardCheck boardCheck)
+        public override bool CanHandle(IPieceState pieceState, IMoveCheck moveCheck)
         {
-            if (validator.IsPiece(this))
+            if (pieceState.PieceType == PieceType && pieceState.Player == Player)
             {
-                var path = validator.Moves.Where(x => x.End == Index);
+                var path = pieceState.Moves.Where(x => x.End == Index);
 
-                return path.Any() 
-                    && (boardCheck.IsClear(path.Single()) ? true : 
-                    throw new UserMoveException(this, "Illegal move due to unclear path!"));
+                return path.Any() && (path.Any() && moveCheck.IsClear(path.Single()));
             }
 
             return false;
+        }
+
+        public override void Perform(IMovePerform boardMove)
+        {
+            var current = boardMove.Performers.Single();
+
+            Action(current, Index);
+
+            current.Update(this);
         }
     }
 }
