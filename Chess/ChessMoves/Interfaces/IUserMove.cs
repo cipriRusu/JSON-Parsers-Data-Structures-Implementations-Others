a36d3e968a1234513d44b54799c4795eb5ac82d2
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-07 08:19:27 +0300

    Implemented IUserMove members

--- Chess/ChessMoves/Interfaces/IUserMove.cs
+++ Chess/ChessMoves/Interfaces/IUserMove.cs
@@ -1,10 +1,13 @@
 ﻿using ChessGame.Interfaces;
 using System;
+using System.Collections.Generic;
 
 namespace ChessMoves
 {
     public interface IUserMove : ILocation, IPlayer, INotation, IPieceType
     {
-        bool CanHandle(IValidate validator, IBoardCheck boardCheck);
+        void CallBack(Action<IPiece, (int, int)> action);
+        bool CanHandle(IPieceState pieceStats, IMoveCheck boardCheck);
+        void Perform(IMovePerform boardMove);
     }
 }
