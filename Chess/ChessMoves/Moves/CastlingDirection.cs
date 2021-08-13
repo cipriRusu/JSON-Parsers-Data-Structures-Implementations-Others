44992f438757ab8ce3f7175ecb6ec93eb964c4bb
Cipri Rusu <cipri91rusu@gmail.com>
2021-08-13 11:26:51 +0300

    Moved classes accordingly

---
+++ Chess/ChessMoves/Moves/CastlingDirection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame
+{
+    public enum CastlingDirection
+    {
+        QueenSide,
+        KingSide,
+    }
+}
