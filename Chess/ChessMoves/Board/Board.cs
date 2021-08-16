015b52e34b1267e11bc42dc0580aca2984a42343
Cipri Rusu <cipri91rusu@gmail.com>
2021-08-17 01:21:28 +0300

    Added check and checkmate verification into board (no error checking and corner cases)

--- Chess/ChessMoves/Board/Board.cs
+++ Chess/ChessMoves/Board/Board.cs
@@ -1,4 +1,5 @@
 ﻿using ChessGame.Interfaces;
+using ChessMoves.Moves;
 using ChessMoves.Paths;
 using System;
 using System.Collections.Generic;
@@ -12,15 +13,66 @@ namespace ChessMoves
         private IPiece[,] board = new IPiece[ChessboardSize, ChessboardSize];
         public Board(IPiece[,] board) => this.board = board;
         public bool IsClear(IPath path) => path.Path.All(x => board[x.Item1, x.Item2] == null);
+        public bool IsCheck { get; private set; }
+        public bool IsCheckMate { get; private set; }
         public IEnumerable<IPiece> Performers { get; private set; }
 
         public void GetCurrent(IUserMove move)
         {
-            Performers = AllPieces().Where(x => x != null && x.CanPerform(move, this));
+            Performers = AllPieces().Where(x => 
+            x != null && 
+            x.CanPerform(move, this) && 
+            new MoveConstraintValidator(x, move).IsValid);
 
             move.CallBack(MoveTo);
 
             move.Perform(this);
+
+            if (IsCheck == false && move is KingCheckUserMove)
+            {
+                var performer = board[move.Index.Item1, move.Index.Item2];
+
+                var pathToKing = performer.Captures.Where(x => 
+                x != null &&
+                board[x.End.Item1, x.End.Item2] is King && 
+                board[x.End.Item1, x.End.Item2].Player != move.Player);
+                
+                try
+                {
+                    var isClear = IsClear(pathToKing.Single());
+                    IsCheck = pathToKing != null && isClear;
+                }
+                catch (Exception)
+                {
+                    throw new UserMoveException(move, "Invalid check move");
+                }
+            }
+
+            if(IsCheckMate == false && move is KingCheckMateUserMove)
+            {
+                var performer = board[move.Index.Item1, move.Index.Item2];
+
+                var pathToKing = performer.Captures.Where(x =>
+                x != null &&
+                board[x.End.Item1, x.End.Item2] is King &&
+                board[x.End.Item1, x.End.Item2].Player != move.Player);
+
+                IsCheckMate = true;
+            }
+
+            if(IsCheck == true && !(move is KingCheckUserMove)) {
+
+                var currentKing = AllPieces().Where(x =>
+                x != null &&
+                x is King &&
+                x.Player == move.Player).Single();
+
+                var opponents = AllPieces().Where(x =>
+                x != null &&
+                x.Player != move.Player);
+                
+                IsCheck = false;
+            }
         }
 
         private void MoveTo(IPiece piece, (int, int) Target)
@@ -32,7 +84,7 @@ namespace ChessMoves
         private IEnumerable<IPiece> AllPieces()
         {
             var allIndices = Enumerable.Range(0, ChessboardSize);
-
+    
             return allIndices.SelectMany(x => allIndices.Select(y => board[x, y]));
         }
     }
